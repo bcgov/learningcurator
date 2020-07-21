a6bda0bc9abc4660e4d841d3ac463e48b69c12be
Allan Haggett <allan.haggett@gov.bc.ca>
2020-07-21 13:34:33 -0700

    Add in proper favicon support

--- templates/layout/nowrap.php
+++ templates/layout/nowrap.php
@@ -25,6 +25,31 @@ Wanna go from getting a 60 on peformance in Lighthouse to a 97?
 <link href="/learning-curator/css/home.css" rel="stylesheet"> 
 <link href="/learning-curator/fontawesome/css/all.css" rel="stylesheet"> 
 
+
+<!-- ****** faviconit.com favicons ****** -->
+<link rel="shortcut icon" href="/learning-curator/favicon/favicon.ico">
+<link rel="icon" sizes="16x16 32x32 64x64" href="/learning-curator/favicon/favicon.ico">
+<link rel="icon" type="image/png" sizes="196x196" href="/learning-curator/favicon/favicon-192.png">
+<link rel="icon" type="image/png" sizes="160x160" href="/learning-curator/favicon/favicon-160.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/learning-curator/favicon/favicon-96.png">
+<link rel="icon" type="image/png" sizes="64x64" href="/learning-curator/favicon/favicon-64.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/learning-curator/favicon/favicon-32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/learning-curator/favicon/favicon-16.png">
+<link rel="apple-touch-icon" href="/learning-curator/favicon/favicon-57.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/learning-curator/favicon/favicon-114.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/learning-curator/favicon/favicon-72.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/learning-curator/favicon/favicon-144.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/learning-curator/favicon/favicon-60.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/learning-curator/favicon/favicon-120.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/learning-curator/favicon/favicon-76.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/learning-curator/favicon/favicon-152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/learning-curator/favicon/favicon-180.png">
+<meta name="msapplication-TileColor" content="#FFFFFF">
+<meta name="msapplication-TileImage" content="/learning-curator/favicon/favicon-144.png">
+<meta name="msapplication-config" content="/learning-curator/browserconfig.xml">
+<!-- ****** faviconit.com favicons ****** -->
+
+
 </head>
 <body class="" data-spy="scroll" data-target="#stepnav" data-offset="110">
 <?php 
