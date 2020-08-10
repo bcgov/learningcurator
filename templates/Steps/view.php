39e2f8b334270a269e994d0dcf56f36e5be0d466
Allan Haggett <allan.haggett@gov.bc.ca>
2020-08-10 13:49:43 -0700

    Make step progress work with new sticky-top nav

--- templates/Steps/view.php
+++ templates/Steps/view.php
@@ -216,7 +216,7 @@ $lastobj = $s->description;
 </div>
 </div>
 
-<div class="progress progress-bar-striped stickyprogress" style="background-color: #F1F1F1; border-radius: 0; height: 13px;">
+<div class="progress progress-bar-striped stickyprogress" style="background-color: #F1F1F1; border-radius: 0; height: 18px;">
 		<div class="progress-bar" role="progressbar" style="background-color: rgba(88,174,36,1); color: #FFF; width: <?= $steppercent ?>%" aria-valuenow="<?= $steppercent ?>" aria-valuemin="0" aria-valuemax="100">
 		This step is <?= $steppercent ?>% done
 	  </div>
@@ -421,7 +421,7 @@ $lastobj = $s->description;
 </div>
 <div class="col-8 col-md-3 col-lg-2">
 <?php if(in_array($uid,$usersonthispathway)): ?>
-<div class="p-3 bg-white mb-3 text-center stickyrings">
+<div class="p-3 bg-white mb-3 text-center stickyrings rounded-lg">
 <div class="mb-3 following"></div>
 <canvas id="myChart" width="250" height="250"></canvas>
 </div>
@@ -600,7 +600,7 @@ function loadStatus() {
 			if(stepstatus.steppercent == 100) {
 				$('.finish').removeClass('hide').addClass('show');
 			}
-			$('.progress-bar').width(stepstatus.steppercent+'%').html(stepstatus.steppercent+'% completed');
+			$('.progress-bar').width(stepstatus.steppercent+'%').html('This step is ' + stepstatus.steppercent + '% completed');
 			
 		},
 		statusCode: 
