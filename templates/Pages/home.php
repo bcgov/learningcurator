9718bc042812d08a87ce83fef71b58f07c0f3a96
Allan Haggett <allan.haggett@gov.bc.ca>
2020-08-19 11:47:01 -0700

    Home page dip/deeper/dive redesign

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -34,9 +34,9 @@
 	onboarding and development of their employees.</div>
 </div>	
 </div>
-</div>
+</div> <!-- /.row -->
 
-</div>
+</div> <!-- /.pad-lg -->
 
 <div class="text-center">
 	<a href="#activitytypes">
@@ -44,8 +44,149 @@
 			style="color: #000; font-size: 300%; margin: 0 0 30px 0;"></i>
 	</a>
 </div>
+</div> <!-- /.col-12 -->
+</div> <!-- /.row -->
+
+
+
+
+
+
+
+<div class="row justify-content-md-center bg-white align-items-center linear" id="dipdivedeeper">
+<div class="col">
+<div class="pad-lg">
+
+	<h3 class="mb-3">
+		<i class="fas fa-thermometer-quarter" style="font-size: 300%"></i>
+		Dip your toe in the pool
+	</h3>
+	
+	<div class="row mt-3">
+	<div class="col-md-4">
+	<div class="pad-sm bg-white rounded-lg mb-2">
+		<h4>The Latest Additions</h4>
+		<p>Check out the latest activities that have been added to the site.
+		Curators are continually adding new resources and creating new pathways
+		in various categories.</p>
+		<a class="btn btn-lg btn-dark btn-block" href="/learning-curator/activities">The Latest Stuff</a>
+		</div> <!-- /.pad-sm -->
+	</div>
+	<div class="col-md-4">
+	<div class="pad-sm bg-white rounded-lg mb-2">
+		<h4>Search for something</h4>
+		<p>Looking for something specific? Type it into the box and see if we already 
+		have it included on some path. Explore the links!</p>
+		<form method="get" action="/learning-curator/activities/find" class="bg-dark rounded-lg p-2 form-inline">
+			<input class="form-control " type="search" placeholder="Activity Search" aria-label="Search" name="q">
+			<button class="btn btn-light" type="submit">Search</button>
+		</form>
+		</div> <!-- /.pad-sm -->
+	</div>
+	<div class="col-md-4">
+	<div class="pad-sm bg-white rounded-lg mb-2">
+		<h4>Bookmarks</h4>
+		<p>See something you like? Bookmark it and come back to it later without having to dig 
+		around. This is a great way to use the site, while not going any deeper.</p>
+		<a class="btn btn-lg btn-dark btn-block" href="/learning-curator/users/bookmarks">Bookmarks</a>
+	</div> <!-- /.pad-sm -->
+	
+	</div>
+	</div> <!-- /.row -->
+
+
+
+
+
+	<h3 class="my-3">
+		<i class="fas fa-thermometer-half" style="font-size: 300%"></i>
+		Go a bit deeper
+	</h3>
+	
+	<div class="row mt-3">
+	<div class="col-md-4">
+	<div class="pad-sm bg-white rounded-lg mb-2">
+		<h4>Your Profile</h4>
+		<p>Your profile shows you all of your activity in the site: 
+		pathways you're following; acitvities you've claimed; 
+		your bookmarks, and reports made.</p>
+		<a class="btn btn-lg btn-dark btn-block" href="/learning-curator/users/pathways">Your Profile</a>
+	</div> <!-- /.pad-sm -->
+	</div>
+	<div class="col-md-4">
+	<div class="pad-sm bg-white rounded-lg mb-2">
+		<h4>Follow Pathways</h4>
+		<p>Following a pathway is a commitment look at each step, claim its
+		required activities after you complete them, and to consider the supplemental.
+		Pathways are organized around common organizational goals.</p>
+		<a class="btn btn-lg btn-dark btn-block" href="/learning-curator/users/pathways">Your Pathways</a>
+	</div> <!-- /.pad-sm -->
+	</div>
+	<div class="col-md-4">
+	<div class="pad-sm bg-white rounded-lg mb-2">
+		<h4>Claim Activities</h4>
+		<p>In order to track your progress through a pathway, you're 
+		supposed to "Claim" the activities in pathway steps, and do so 
+		approximately in the order indicated. Each step has a goal, and 
+		you work progressively towards it.</p>
+		<a class="btn btn-lg btn-dark btn-block" href="/learning-curator/users/claimed">Your Activities</a>
+	</div> <!-- /.pad-sm -->
+	</div>
+	</div> <!-- /.row -->
+
+
+
+
+
+
+
+	<h3 class="mt-3">
+	<i class="fas fa-thermometer-full" style="font-size: 300%"></i>
+	Dive in and fully engage</h3>
+	
+	<div class="row mt-3">
+	<div class="col-md-4">
+	<div class="pad-sm bg-white rounded-lg mb-2">
+		<h4>MyPerformance Goals</h4>
+		<p>Talk to your supervisor/manager at a 1:1 and let them know
+		that you're following a pathway and incorporate your progress
+		into your yearly goals.</p>
+	</div> <!-- /.pad-sm -->
+	</div>
+	<div class="col-md-4">
+	<div class="pad-sm bg-white rounded-lg mb-2">
+		<h4>Report!</h4>
+		<p>Is something not quite right? Is that link dead? Has that activity
+		been replaced with something else? There's a "Report" button on every
+		activity. Let us know!</p>
+
+	</div> <!-- /.pad-sm -->
+	</div>
+	<div class="col-md-4">
+	<div class="pad-sm bg-white rounded-lg mb-2">
+		<h4>Contribute</h4>
+		<p>Do you know of an activity that would be perfect for a pathway? Let us know by 
+		<a href="/learning-curator/activities/contribute">filling out the form</a> and suggesting it to our curator team. We'll review your 
+		contribution and collaborate with you. </p>
+		<!-- <a class="btn btn-lg btn-dark btn-block" href="/learning-curator/activities/contribute">Suggest an Activity</a> -->
+	</div> <!-- /.pad-sm -->
+	</div>
+	</div> <!-- /.row -->
+
+
+
+
+</div> <!-- /.pad-lg -->
+</div> <!-- /.col -->
+</div> <!-- /.row -->
+
+
+
+
 </div>
 </div>
+
+
 
 
 
@@ -54,7 +195,8 @@
 <?php foreach($atypes as $type): ?>
 <?php $count++ ?>
 	<div class="col-md-6" style="background-color: rgba(<?= $type->color ?>,.2)">
-	<div class="m-3 p-3">
+	<div class="pad-md">
+	<div class="pad-sm bg-white rounded-lg mb-2">
 	<div class="mb-3">
 		<a href="/learning-curator/activity-types/view/<?= $type->id ?>" class="activity-icon activity-icon-lg" style="background-color: rgba(<?= $type->color ?>,1)">
 			<i class="activity-icon activity-icon-lg fas <?= $type->image_path ?>"></i>
@@ -68,6 +210,7 @@
 		</div>
 	</div>
 	</div>
+	</div>
 <?php endforeach ?>
 </div>
 
@@ -75,46 +218,6 @@
 
 
 
-<div class="row justify-content-md-center bg-white align-items-center linear" id="dipdivedeeper">
-
-<div class="col-md-10">
-
-<div class="sectiontext">
-	
-
-<div class="row">
-<div class="col-md-4">
-	
-	<h3>
-	<i class="fas fa-thermometer-quarter" style="font-size: 300%"></i>
-	Dip</h3>
-	<p>Just dip your toe in the pool to see if there's anything that can inspire you.</p>
-	
-	<a class="btn btn-lg btn-dark btn-block" href="/learning-curator/activities">The Latest Stuff</a>
-	
-</div>
-<div class="col-md-4">
-	
-	<h3>
-	<i class="fas fa-thermometer-half" style="font-size: 300%"></i>
-	Deeper</h3>
-	<p>Go a bit deeper and look into following a pathway and tracking your progress.</p>
-	
-	<a class="btn btn-lg btn-dark btn-block" href="/learning-curator/users/pathways">Check out your Profile</a>
-	
-
-</div>
-<div class="col-md-4">
-	
-	<h3>
-	<i class="fas fa-thermometer-full" style="font-size: 300%"></i>
-	Dive</h3>
-	<p>Go all in and set a MyPerformance goal to complete your pathways.</p>
-	<a class="btn btn-lg btn-dark btn-block" href="/learning-curator/users/home">Read More</a>
-</div>
-</div>
-
-
 </div>
 </div>
 </div>
@@ -123,7 +226,7 @@
 
 
 
-</div>
+
 
 
 
