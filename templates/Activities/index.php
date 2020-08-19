fb7ad6bd81212c8bad115a16229ee01ccde4b575
Allan Haggett <allan.haggett@gov.bc.ca>
2020-08-19 10:01:39 -0700

    Tweak latest to remove like counts Step link goes to step instead of path General layout/spacing tweaks

--- templates/Activities/index.php
+++ templates/Activities/index.php
@@ -40,7 +40,7 @@ if ($this->Identity->isLoggedIn()) {
 <div class="">
 <?php foreach ($activities as $activity): ?>
 
-<div class="p-3 rounded-lg mb-2 bg-white"> <!-- style="background-color: rgba(<?= $activity->activity_type->color ?>,.2)" -->
+<div class="p-3 rounded-lg mb-3 bg-white"> <!-- style="background-color: rgba(<?= $activity->activity_type->color ?>,.2)" -->
 <div class="activity-icon activity-icon-md" style="background-color: rgba(<?= $activity->activity_type->color ?>,1)">
 		<i class="activity-icon activity-icon-md fas <?= $activity->activity_type->image_path ?>"></i>
 	</div>
@@ -49,69 +49,36 @@ if ($this->Identity->isLoggedIn()) {
 	<?= $this->Html->link($activity->name, ['action' => 'view', $activity->id]) ?>
 </h3>
 <div class="">
+	<div class="py-3">
 	<?= $activity->description ?>
-	<div class="mt-2"><span class="lcount"><?= h($activity->recommended) ?></span> <i class="fas fa-thumbs-up"></i></div>
+	</div>
+	<div class="pb-3">
+	<?php foreach($activity->steps as $step): ?>
+	<?php foreach($step->pathways as $path): ?>
+	<?php if($path->status_id == 2): ?>
+	<span class="badge badge-light"><a href="/learning-curator/steps/view/<?= $step->id ?>"><?= $path->name ?> - <?= $step->name ?></a></span>
+	<?php endif ?>
+	<?php endforeach ?>
+	<?php endforeach ?>
+	</div>
 	<?php if(!in_array($activity->id,$userbooklist)): // if the user hasn't bookmarked this, then show them claim form ?>
 	<?= $this->Form->create(null,['url' => ['controller' => 'activities-bookmarks', 'action' => 'add'], 'class' => 'bookmark form-inline']) ?>
 		<?= $this->Form->hidden('activity_id',['value' => $activity->id]) ?>
-		<button class="btn btn-light"><i class="fas fa-bookmark"></i> Bookmark</button>
+		<button class="btn btn-dark"><i class="fas fa-bookmark"></i> Bookmark</button>
 		<?php //$this->Form->button(__('Bookmark'),['class' => 'btn btn-light']) ?>
 		<?= $this->Form->end() ?>
 	<?php else: ?>
 		<span class="btn btn-dark"><i class="fas fa-bookmark"></i> Bookmarked</span>
 	<?php endif ?>
-	<div class="mt-2" style="font-size: 12px">Added on <?= $activity->created ?></div>
+	<div class="mt-3"><span class="badge badge-light">Added: <?= h($activity->created) ?></span></div>
 
 </div>
 
 
-<?php if($role == 2 || $role == 5): ?>
-<button class="btn btn-light btn-sm" type="button" data-toggle="collapse" data-target="#assignment<?= $activity->id ?>" aria-expanded="false" aria-controls="assignment<?= $activity->id ?>">
-    Path Assigment
-  </button>
-<?php endif ?>
-<?php foreach($activity->steps as $step): ?>
-<?php foreach($step->pathways as $path): ?>
-<?php if($path->status_id == 2): ?>
-<span class="badge badge-light"><a href="/learning-curator/pathways/view/<?= $path->id ?>"><?= $path->name ?> - <?= $step->name ?></a></span>
-<?php endif ?>
-<?php endforeach ?>
-<?php endforeach ?>
-
-
-<?php if($role == 2 || $role == 5): ?>
-<div class="collapse" id="assignment<?= $activity->id ?>">
-<?php foreach($allpathways as $pathway): ?>
-<div class="my-1 p-3" style="background-color: rgba(255,255,255,.3)">
-<button class="btn btn-light btn-sm" type="button" data-toggle="collapse" data-target="#steps<?= $pathway->id ?>" aria-expanded="false" aria-controls="steps<?= $pathway->id ?>">
-    Steps
-  </button>
-  <?php if($pathway->status_id != 2): ?>
-  <span class="badge badge-light"><?= $pathway->status->name ?></span> 
-  <?php endif ?>
-  <a href="/learning-curator/pathways/view/<?= $pathway->id ?>"><?= $pathway->name ?></a>
-
-<div class="collapse p-3" id="steps<?= $pathway->id ?>">
-<?= $this->Form->create(null, ['url' => ['controller' => 'activities-steps','action' => 'add', 'class' => '']]) ?>
-<?= $this->Form->control('pathway_id',['type' => 'hidden', 'value' => $pathway->id ]) ?>
-<?= $this->Form->control('activity_id',['type' => 'hidden', 'value' => $activity->id]) ?>
-<?php foreach($pathway->steps as $step): ?>
-<label style="display: inline-block; margin: 0 10px 0 5px;">
-<input id="step_id_<?= $step->id ?>" type="radio" name="step_id" value="<?= $step->id ?>">
-<?= $step->name ?>
-</label>
-<?php endforeach ?>
-<?= $this->Form->button(__('Assign'),['class'=>'btn btn-sm btn-light']) ?>
-<?= $this->Form->end() ?>
-</div>
-</div>
-<?php endforeach ?>
-</div>
-<?php endif ?>
 
 </div>
 <?php endforeach; ?>
-</div> <!-- /.card-columns -->
+</div> <!-- /. -->
 
 
 
@@ -125,7 +92,7 @@ if ($this->Identity->isLoggedIn()) {
 <?php foreach($allpathways as $path): ?>
 <?php if($path->status_id != 2): ?>
 <?php if($role == 2 || $role == 5): ?>
-	<div class="p-3 mb-2 bg-white rounded-lg">
+	<div class="p-3 mb-3 bg-white rounded-lg">
 		<span class="badge badge-warning"><?= $path->status->name ?></span>
 		<h3><a href="/learning-curator/pathways/view/<?= $path->id ?>"><?= $path->name ?></a></h3>
 		<?= $path->objective ?>
@@ -133,7 +100,7 @@ if ($this->Identity->isLoggedIn()) {
 	</div>
 <?php endif ?>
 <?php else: ?>
-	<div class="p-3 mb-2 bg-white rounded-lg">
+	<div class="p-3 mb-3 bg-white rounded-lg">
 		
 		<h3><a href="/learning-curator/pathways/view/<?= $path->id ?>"><?= $path->name ?></a></h3>
 		<?= $path->objective ?>
@@ -148,13 +115,13 @@ if ($this->Identity->isLoggedIn()) {
 <div class="col-md-3">
 <h2 class="mt-3">Latest Categories</h2>
 <?php foreach ($allcats as $cat) : ?>
-<div class="p-3 mb-2 bg-white rounded-lg">
+<div class="p-3 mb-3 bg-white rounded-lg">
 <h3>
 	<?= $this->Html->link($cat->name, ['controller' => 'Categories', 'action' => 'view', $cat->id]) ?>
 </h3>
 <div><?= h($cat->description) ?></div>
 <div class="bg-light p-3 my-3">
-<div class="mb-2"><strong>View pathways on the following topics:</strong></div>
+<div class="mb-3"><strong>View pathways on the following topics:</strong></div>
 <?php foreach($cat->topics as $topic): ?>
 <span class=""><?= $topic->name ?></span>
 <?php endforeach ?>
