8599df1b6932f8a32415e8110a1a194f18a24dd7
Allan Haggett <allan.haggett@gov.bc.ca>
2020-10-20 12:21:24 -0700

    Update user index link in view menu globally

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -138,7 +138,7 @@ if ( strpos($_SERVER['HTTP_USER_AGENT'], 'rv:11.0')     !== false
 			<a class="dropdown-item" href="/learning-curator/pathways">All Pathways</a>
 			<a class="dropdown-item" href="/learning-curator/activity-types">Activity Types</a>
 			<a class="dropdown-item" href="/learning-curator/activities">All Activities</a>
-			<a class="dropdown-item" href="/learning-curator/users">All Users</a>
+			<a class="dropdown-item" href="/learning-curator/users/list">All Users</a>
 			<a class="dropdown-item" href="/learning-curator/competencies">All Competencies</a>
 			<a class="dropdown-item" href="/learning-curator/ministries">All Ministries</a>
 			<a class="dropdown-item" href="/learning-curator/categories">All Categories</a>
