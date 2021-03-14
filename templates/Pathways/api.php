c0434536786ab973afe8a9e38d22a10babe82f95
Allan Haggett <allan.haggett@gov.bc.ca>
2021-03-14 12:54:08 -0700

    Update Pathway API call to be valid JSON

--- templates/Pathways/api.php
+++ templates/Pathways/api.php
@@ -1,3 +1,12 @@
 <?php 
 $this->layout = 'ajax';
-echo json_encode($pathway);
+?>
+<pre>
+{
+"version": "https://jsonfeed.org/version/1",
+"title": "Learning Curator individual pathway listing all steps and contained activities.",
+"home_page_url": "https://learningcentre.gww.bc.ca",
+"feed_url": "https://learningcentre.gww.bc.ca",
+"items": 
+<?= json_encode($pathway) ?>
+}
