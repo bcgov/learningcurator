a19728a704d5b186bbf2e024d4c94d518ce74ae5
Allan Haggett <allan.haggett@gov.bc.ca>
2021-05-24 18:53:34 -0700

    Activity types template

--- templates/ActivityTypes/index.php
+++ templates/ActivityTypes/index.php
@@ -3,60 +3,71 @@
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\ActivityType[]|\Cake\Collection\CollectionInterface $activityTypes
  */
+$count = 0;
 ?>
-<div class="activityTypes index content">
-    <?= $this->Html->link(__('New Activity Type'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Activity Types') ?></h3>
-    <div class="table-responsive">
-        <table>
-            <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('name') ?></th>
-                    <th><?= $this->Paginator->sort('slug') ?></th>
-                    <th><?= $this->Paginator->sort('color') ?></th>
-                    <th><?= $this->Paginator->sort('delivery_method') ?></th>
-                    <th><?= $this->Paginator->sort('image_path') ?></th>
-                    <th><?= $this->Paginator->sort('featured') ?></th>
-                    <th><?= $this->Paginator->sort('created') ?></th>
-                    <th><?= $this->Paginator->sort('createdby') ?></th>
-                    <th><?= $this->Paginator->sort('modified') ?></th>
-                    <th><?= $this->Paginator->sort('modifiedby') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($activityTypes as $activityType): ?>
-                <tr>
-                    <td><?= $this->Number->format($activityType->id) ?></td>
-                    <td><?= h($activityType->name) ?></td>
-                    <td><?= h($activityType->slug) ?></td>
-                    <td><?= h($activityType->color) ?></td>
-                    <td><?= h($activityType->delivery_method) ?></td>
-                    <td><?= h($activityType->image_path) ?></td>
-                    <td><?= $this->Number->format($activityType->featured) ?></td>
-                    <td><?= h($activityType->created) ?></td>
-                    <td><?= h($activityType->createdby) ?></td>
-                    <td><?= h($activityType->modified) ?></td>
-                    <td><?= h($activityType->modifiedby) ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $activityType->id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $activityType->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $activityType->id], ['confirm' => __('Are you sure you want to delete # {0}?', $activityType->id)]) ?>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+<script>
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+//alert(rgbToHex(0, 51, 255)); // #0033ff
+</script>
+
+<div class="container-fluid">
+<div class="row justify-content-md-center bg-white" id="activitytypes">
+<?php foreach($activityTypes as $type): ?>
+<?php $count++ ?>
+	<div class="col-md-6" style="background-color: rgba(<?= $type->color ?>,.2)">
+	<div class="pad-md">
+	<div class="pad-sm bg-white rounded-lg mb-2">
+	<div class="mb-3">
+    <div class="float-right"><?= $this->Html->link(__('Edit'), ['action' => 'edit', $type->id],['class' => 'btn btn-light mt-3']) ?></div>
+		<a href="/activity-types/view/<?= $type->id ?>" class="activity-icon activity-icon-lg" style="background-color: rgba(<?= $type->color ?>,1)">
+			<i class="activity-icon activity-icon-lg fas <?= $type->image_path ?>"></i>
+		</a>
+		<a href="/activity-types/view/<?= $type->id ?>" class="" style="color: #333; font-size: 230%">
+			<?= h($type->name) ?>
+		</a>
+	</div>
+		<div class="mb-3" style="font-size: 120%;">
+		<?= $type->description ?>
+		</div>
+        <div>
+        RGB: <?= h($type->color) ?><br>
+        Hex: <span class="text-uppercase hexx<?= $count ?>"></span>
+        <script> 
+        var hoo<?= $count ?> = rgbToHex(<?= h($type->color) ?>);
+        console.log(hoo<?= $count ?>);
+        document.querySelector('.hexx<?= $count ?>').innerHTML = hoo<?= $count ?>;
+        </script>
+        
     </div>
-    <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+    <div>
+    <a href="https://fontawesome.com/icons?d=gallery&m=free" target="_blank" rel="noopener">
+        FontAwesome Icon</a>: <?= h($type->image_path) ?>
     </div>
+
+    
+	</div>
+	</div>
+	</div>
+<?php endforeach ?>
 </div>
+</div>
+
+
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" 
+	integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" 
+	crossorigin="anonymous"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/js/bootstrap.min.js" 
+	integrity="sha384-3qaqj0lc6sV/qpzrc1N5DC6i1VRn/HyX4qdPaiEFbn54VjQBEU341pvjz7Dv3n6P" 
+	crossorigin="anonymous"></script>
+
