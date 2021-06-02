95a72d37c0b012b372980c2b023e9bd82827aed6
Allan Haggett <allan.haggett@gov.bc.ca>
2021-06-01 17:52:20 -0700

    Add azure mapper and profiletrait files for instal copy

---
+++ config/tocopy/AzureMapper.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Copyright 2010 - 2019, Cake Development Corporation (https://www.cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2010 - 2019, Cake Development Corporation (https://www.cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+namespace CakeDC\Auth\Social\Mapper;
+
+use Cake\Utility\Hash;
+
+/**
+ * Azure Mapper
+ */
+class Azure extends AbstractMapper
+{
+    /**
+     * Map for provider fields
+     *
+     * @var array
+     */
+    protected $_mapFields = [
+        'id' => 'email',
+        'email' => 'email',
+        'first_name' => 'full_name',
+        'last_name' => 'full_name'
+    ];
+
+    /**
+     * Get link property value
+     *
+     * @param mixed $rawData raw data
+     * @return string
+     */
+    protected function _link($rawData)
+    {
+        return Hash::get($rawData, $this->_mapFields['link']) ?: '#';
+    }
+    
+}
