6fcb6260e328f0b9a2026f1d732b8f57bcdd467d
Allan Haggett <allan.haggett@gov.bc.ca>
2021-06-15 15:51:25 -0700

    Start to support tags again

--- src/Model/Entity/Activity.php
+++ src/Model/Entity/Activity.php
@@ -88,4 +88,18 @@ class Activity extends Entity
         'steps' => true,
         'tags' => true,
     ];
+    protected function _getTagString()
+    {
+        if (isset($this->_fields['tag_string'])) {
+            return $this->_fields['tag_string'];
+        }
+        if (empty($this->tags)) {
+            return '';
+        }
+        $tags = new Collection($this->tags);
+        $str = $tags->reduce(function ($string, $tag) {
+            return $string . $tag->name . ', ';
+        }, '');
+        return trim($str, ', ');
+    }
 }
