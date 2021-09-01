41e73787b8d39e9f22f6eaea58406732b4f38384
Allan Haggett <allan.haggett@gov.bc.ca>
2021-09-01 14:08:15 -0700

    Update pathway index so that curator view of followers is not its own column

--- templates/Pathways/view.php
+++ templates/Pathways/view.php
@@ -139,30 +139,7 @@ This seems to work out, but #TODO investigate optimizing this
 </div>
 <div class="container-fluid linear">
 <div class="row justify-content-md-center">
-<?php if($role == 'curator' || $role == 'superuser'): ?>
-<div class="col-md-3 col-lg-2 order-last">
-<div class="bg-white rounded-lg p-3 my-3">
-<a class="" 
-	data-toggle="collapse" 
-	href="#followerlist" 
-	role="button" 
-	aria-expanded="false" 
-	aria-controls="collapseExample">
-		<span class="badge badge-pill badge-dark"><?= $totalusers ?></span> 
-		people are following this path
-</a>
-<div class="collapse" id="followerlist">
-<ul class="list-group list-group-flush">
-<?php foreach($followers as $follower): ?>
-<li class="list-group-item">
-	<a href="/users/view/<?= $follower[0] ?>"><?= $follower[1] ?></a>
-</li>
-<?php endforeach ?>
-</ul>
-</div>
-</div>
-</div>
-<?php endif ?>
+
 <div class="col-6 col-md-3 col-lg-2 order-last">
 
 <?php if(in_array($uid,$usersonthispathway)): ?>
@@ -192,12 +169,43 @@ This seems to work out, but #TODO investigate optimizing this
 	
 	Following a pathway allows you to track your progress and pin the pathway to <a href="/profile">your profile</a>.
 
+
+
+
+
 </div>
 <!--When you select to follow a pathway, this pathway will show as a journey you are on and may be 
 accessed from your profile page. Think of it as “bookmarking” learning you want to come back to and track your progress on.-->
 
+
 </div>
 </div>
+
+<?php if($role == 'curator' || $role == 'superuser'): ?>
+<div class="bg-white rounded-lg p-3 my-3">
+<a class="" 
+	data-toggle="collapse" 
+	href="#followerlist" 
+	role="button" 
+	aria-expanded="false" 
+	aria-controls="collapseExample">
+		<span class="badge badge-pill badge-dark"><?= $totalusers ?></span> 
+		people are following this path
+</a>
+<div class="collapse" id="followerlist">
+<ul class="list-group list-group-flush">
+<?php foreach($followers as $follower): ?>
+<li class="list-group-item">
+	<a href="/users/view/<?= $follower[0] ?>"><?= $follower[1] ?></a>
+</li>
+<?php endforeach ?>
+</ul>
+</div>
+</div>
+<?php endif ?>
+
+
+
 <?php endif ?>
 
 <!--<div class="" style="font-size: 12px">Published <?= h(date('D M jS \'y',strtotime($pathway->created))) ?></div>-->
