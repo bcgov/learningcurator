8cdfd8894e2cae3407b409c7de1a8b3614286abe
Allan Haggett <allan.haggett@gov.bc.ca>
2021-11-03 15:34:58 -0700

    Ministry add form format

--- templates/Ministries/add.php
+++ templates/Ministries/add.php
@@ -4,31 +4,35 @@
  * @var \App\Model\Entity\Ministry $ministry
  */
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Ministries'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="ministries form content">
-            <?= $this->Form->create($ministry) ?>
-            <fieldset>
-                <legend><?= __('Add Ministry') ?></legend>
-                <?php
-                    echo $this->Form->control('name');
-                    echo $this->Form->control('slug');
-                    echo $this->Form->control('elm_learner_group');
-                    echo $this->Form->control('description');
-                    echo $this->Form->control('hyperlink');
-                    echo $this->Form->control('image_path');
-                    echo $this->Form->control('color');
-                    echo $this->Form->control('featured');
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
-        </div>
-    </div>
+<div class="container-fluid" id="colorful">
+<div class="row justify-content-md-center">
+<div class="col-md-6">
+<div class="m-5 p-5 bg-white rounded-lg shadow-sm">
+    <h1>Add Ministry</h1>
+</div>
+</div>
+</div>
+</div>
+<div class="container-fluid">
+<div class="row justify-content-md-center">
+<div class="col-md-6">
+<div class="m-5 p-5 bg-white rounded-lg shadow-sm">
+<?= $this->Form->create($ministry) ?>
+<fieldset>
+<?php
+echo $this->Form->control('name',['class' => 'form-control']);
+echo $this->Form->control('slug',['class' => 'form-control','label'=>'Shortcode']);
+echo $this->Form->hidden('elm_learner_group',['value'=>'ALLBCGOV']);
+echo $this->Form->control('description',['class' => 'form-control']);
+//echo $this->Form->control('hyperlink');
+//echo $this->Form->control('image_path');
+//echo $this->Form->control('color');
+//echo $this->Form->control('featured');
+?>
+</fieldset>
+<?= $this->Form->button(__('Add Ministry'),['class' => 'btn btn-success mt-1']) ?>
+<?= $this->Form->end() ?>
+</div>
+</div>
+</div>
 </div>
