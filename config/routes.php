4b0b18bc37f1c539b40a6ca6f468910dc5cd9db1
Allan Haggett <allan.haggett@gov.bc.ca>
2021-11-04 09:11:10 -0700

    Start a pathway JSON export option; next import

--- config/routes.php
+++ config/routes.php
@@ -65,6 +65,7 @@ $routes->scope('/', function (RouteBuilder $builder) {
     $builder->connect('/pathways/search', ['controller' => 'Pathways', 'action' => 'search']);
     
     $builder->connect('/pathways/{slug}', ['controller' => 'Pathways', 'action' => 'view'])->setPass(['slug']);
+    $builder->connect('/pathways/{slug}/export', ['controller' => 'Pathways', 'action' => 'export'])->setPass(['slug']);
     $builder->connect('/pathways/{pathslug}/s/{stepid}/{stepslug}', ['controller' => 'Steps', 'action' => 'view'])->setPass(['stepid']);
 
     $builder->connect('/profile/claims', ['controller' => 'ActivitiesUsers', 'action' => 'claims']);
