65a59bd2f6eedec0fd69c579077c01f55b7f16f7
Allan Haggett <allan.haggett@gov.bc.ca>
2022-01-23 13:02:03 -0800

    Formatting

--- templates/Activities/index.php
+++ templates/Activities/index.php
@@ -22,23 +22,17 @@ if ($this->Identity->isLoggedIn()) {
 	color; #FFF;
 }
 </style>
-<div class="dark:text-white">
-<h1>Activities</h1>
-<p>The 30 most recently added activities.</p>
-<form method="get" action="/activities/find" class="form-inline my-2 my-lg-0 mr-3" role="search">
-		<input class="form-control mr-sm-2" type="search" placeholder="Activity Search" aria-label="Search" name="search">
-		<button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
-	</form>
-</div>
+<div class="p-6 dark:text-white">
+<h1 class="text-4xl">Activities</h1>
 
 <?php foreach ($activities as $activity) : ?>
 	
 	<div class="p-3 my-3 rounded-lg activity bg-white dark:bg-slate-900 dark:text-white">
 
-	<h3 class="my-3 text-3xl">
+	<h2 class="my-3 text-3xl">
 		<a href="/activities/view/<?= $activity->id ?>"><?= $activity->name ?></a>
 		<!--<a class="btn btn-sm btn-light" href="/activities/view/<?= $activity->id ?>"><i class="fas fa-angle-double-right"></i></a>-->
-	</h3>
+	</h2>
 
 
 
@@ -51,17 +45,43 @@ if ($this->Identity->isLoggedIn()) {
 			</div>
 			<?php endif ?>
 
-		<a target="_blank" 
-			rel="noopener" 
-			data-toggle="tooltip" data-placement="bottom" title="Launch this activity"
-			href="<?= $activity->hyperlink ?>" 
-			class="">
-				Launch
-		</a>
+
+	<div @click.away="open = false" class="relative" x-data="{ open: false }">
+	<button @click="open = !open" class="px-4 py-2 text-lg font-semibold text-right bg-green-700 text-white rounded-lg dark:bg-green-700 dark:focus:text-white dark:hover:text-white dark:focus:bg-gray-600 dark:hover:bg-slate-900 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+		<span>Launch</span>
+		<svg fill="currentColor" viewBox="0 0 8 18" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-8 h-4 transition-transform duration-200 transform md:-mt-1">
+			<path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+		</svg>
+	</button>
+	<div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute z-50 right-0 w-full origin-top-right shadow-lg">
+		<div class="p-4 bg-white rounded-md shadow dark:bg-slate-900">
+
+			<div>
+				<a target="_blank" 
+					rel="noopener" 
+					data-toggle="tooltip" data-placement="bottom" title="Launch this activity"
+					href="<?= $activity->hyperlink ?>" 
+					class="inline-block mb-3 p-3 bg-green-700 rounded-lg text-white text-2xl">
+						Open Activity in new window
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="inline bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+							<path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
+							<path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
+						</svg>
+				</a>
+			</div>
+			<div class="my-4">
+				<?= $activity->hyperlink ?>
+			</div>
+
+		</div>
+	</div>
+	</div>
+		
+
 		
 
 	<?php if(!empty($activity->steps)): ?>
-	<div class="mt-2">
+	<div class="mt-2 p-3 bg-[#003366] rounded-lg">
 
 		<?php foreach($activity->steps as $step): ?>
 			<?php if(!empty($step->pathways[0]->slug)): ?>
@@ -83,3 +103,4 @@ if ($this->Identity->isLoggedIn()) {
 
 	<?php endforeach; // end of activities loop for this step ?>
 
+</div>
