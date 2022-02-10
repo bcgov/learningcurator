3c7f70422c70d849068fb95649a236a4b06fe481
Allan Haggett <allan.haggett@gov.bc.ca>
2022-02-10 10:53:46 -0800

    Update activity launch button hide extraneous info

--- templates/Steps/view.php
+++ templates/Steps/view.php
@@ -190,7 +190,7 @@ Curator says:<br>
 			data-toggle="tooltip" data-placement="bottom" title="Launch this activity"
 			href="/activities-users/launch?activity_id=<?= $activity->id ?>"  
 			class="inline-block mb-3 p-3 bg-emerald-700 rounded-lg text-white text-2xl">
-				Open in a new window
+				Launch in a new window
 				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="inline bi bi-box-arrow-up-right" viewBox="0 0 16 16">
 					<path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
 					<path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
@@ -198,9 +198,29 @@ Curator says:<br>
 		</a>
 	</div>
 
-	<div class="my-4">
-		<?= $activity->hyperlink ?>
+
+
+	<div @click.away="open = false" class="relative" x-data="{ open: false }">
+	<button @click="open = !open" class="px-4 py-2 text-lg font-semibold text-right bg-slate-700 text-white rounded-lg dark:bg-slate-700 dark:focus:text-white dark:hover:text-white dark:focus:bg-gray-600 dark:hover:bg-slate-900 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-slate-200 focus:bg-slate-200 focus:outline-none focus:shadow-outline">
+		<span>More info</span>
+		<svg fill="currentColor" viewBox="0 0 8 18" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-8 h-4 transition-transform duration-200 transform md:-mt-1">
+			<path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+		</svg>
+	</button>
+	<div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="w-full">
+		<div class="p-4 bg-slate-200 rounded-md dark:bg-slate-900">
+
+		<div class="my-4">
+			<?= $activity->hyperlink ?>
+		</div>
+
+
+
+
+		</div>
 	</div>
+</div>
+
 
 
 
