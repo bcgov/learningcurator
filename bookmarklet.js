9d38bf6e1d8f2916da657a5c92d1c138d5b9b34d
Allan Haggett <allan.haggett@gov.bc.ca>
2022-02-23 10:35:34 -0800

    Add window title to bookmarklet

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript: (() => {const destination = "https://learningcurator.ca/activities/addtostep?url=" + window.location.href;window.open(destination);})();
+javascript: (() => {const destination = "https://learningcurator.ca/activities/addtostep?url=" + window.location.href + '&' + window.document.title; window.open(destination);})();
