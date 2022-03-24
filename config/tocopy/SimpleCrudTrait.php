9d4284612ecd676af5efc7a4cb61224a3d0f2bf6
Allan Haggett <allan.haggett@gov.bc.ca>
2022-03-24 16:28:01 -0700

    Top 5 most launched activities

--- config/tocopy/SimpleCrudTrait.php
+++ config/tocopy/SimpleCrudTrait.php
@@ -43,10 +43,54 @@ trait SimpleCrudTrait
         $alphabetized = $table->find('all')->order(['last_name' => 'asc']);
         $tableAlias = $table->getAlias();
         
+        // Get top 5 launched activities and a count for total launches
         $au = TableRegistry::getTableLocator()->get('ActivitiesUsers');
-        $uselaunches = $au->find('all');
+        $uselaunches = $au->find('all')->contain('Activities')->order(['activity_id' => 'asc']);;
+        // can easily grab the total number of launches here
         $launchcount = $uselaunches->count();
+        // setup a few arrays for reducing the massive query results 
+        // down to the top 5
+        $mostlaunched = array();
+        $actnames = array();
+        $links = array();
+        // #TODO This probably isn't a good way of going about this
+        // I'm creating two arrays here and merging them below because
+        // I can't seem to make the count of all activities sort properly
+        // in one go in a multidimensional array (so it includes the 
+        // activity name as well as the ID--needed to make the link), 
+        // so I extract a list that contains the counted and sorted IDs
+        // and then use the 2nd array to extract the names. #inelegantbutworks
+        foreach($uselaunches->toList() as $ul) {
+            array_push($mostlaunched,$ul['activity_id']);
+            array_push($actnames,['actid' => $ul['activity_id'], 'actname' => $ul['activity']['name']]);
+        }
+        $most = array_count_values($mostlaunched);
+        arsort($most);
+        // So far we've counted and sorted the entire list, so now we 
+        // just slice off the top 5. It is probably more efficient to do 
+        // this earlier 
+        $top5 = array_slice($most,0,5,true);
+        foreach($top5 as $id => $count) {
+            // Needing to loop through the entire list of activity launches
+            // for each of the top 5 links to extract the name is probably
+            // a dumb way of doing this, but #inelegantbutworks #refactor
+            foreach($actnames as $key => $val) {
+                if($val['actid'] == $id) {
+                    // #TODO shouldn't be doing formatting here but when I build an array
+                    // the array_unique call below breaks and it took me so long to get to
+                    // this point that I'm just rolling with it for now.
+                    $link = '<a href="/activities/view/' . $id . '">' . $val['actname'] . '</a> <span class="inline-block px-2 py-1 text-xs bg-white dark:bg-black rounded-lg">' . $count . ' launches</span><br>';
+                    array_push($links, $link);
+                }
+            }
+        }
+        // We have a list of the top 5, but each link it repeated for each launch
+        // i.e. if it's launched 11 times, it's listed 11 times
+        // let's reduce that down to one line per activity and we're done!
+        $top5links = array_unique($links);
 
+        // #TODO pull out top 5 followed pathways as per above, but for this
+        // type too...
         $pu = TableRegistry::getTableLocator()->get('PathwaysUsers');
         $pathpins = $pu->find('all');
         $ppincount = $pathpins->count();
--- templates/plugin/CakeDC/Users/Users/index.php
+++ templates/plugin/CakeDC/Users/Users/index.php
@@ -17,11 +17,11 @@
 </div>
 
 <div class="mt-6 mb-2">
-    <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/reports/index">View Reports</a>
-    <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/activity-types">View Activity Types</a>
-    <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/ministries">View Ministries</a>
+    <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/reports/index">Reports</a>
+    <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/activity-types">Activity Types</a>
+    <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/ministries">Ministries</a>
 </div>
-<div class="mb-8">
+<div class="mb-4">
     <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/categories/add">New Category</a>
     <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/topics/add">New Topic</a>
     <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/pathways/add">New Pathway</a> 
@@ -29,7 +29,21 @@
     <!-- <a class="inline-block p-3 my-3 bg-slate-200 dark:bg-sky-700" href="/activities/addtostep">Add Activity to Step</a> -->
 </div>
 
-<form method="get" action="/users/search" class="pt-6 border-t-2 border-slate-900">
+<h2 class="mt-4 text-2xl">Top 5 Launched Activities</h2>
+<div class="p-3 bg-slate-200 dark:bg-slate-900 rounded-lg">
+<ol class="pl-10">
+<?php // these links are templated/built within the controller for noted reasons there
+$count = 4;
+?>
+<?php foreach($top5links as $link): ?>
+    <li class="p-2 list-decimal text-<?= $count ?>xl"><?= $link ?></li>
+    <?php $count-- ?>
+<?php endforeach ?>
+</ol>
+</div>
+
+
+<form method="get" action="/users/search" class="pt-6">
 <label class="">Search for a user:
 <input class="px-3 py-2 m-0 dark:text-white dark:bg-slate-900 rounded-l-lg" 
         type="search" 
@@ -37,10 +51,9 @@
         aria-label="Search" 
         name="q"></label><button class="px-3 py-2 m-0 bg-slate-300 dark:text-white dark:bg-slate-900 dark:hover:bg-slate-800 rounded-r-lg" type="submit">Search</button>
 </form>
-    
 <div class="mt-1 p-3 bg-white dark:bg-slate-900 rounded-lg">
 <table class="w-full">
-    <tr>
+    <tr class="bg-slate-200 dark:bg-slate-800">
         <th>User name</th>
         <th>First name</th>
         <th>Last name</th>
@@ -48,7 +61,7 @@
     </tr>  
 <?php foreach (${$tableAlias} as $user) : ?>
 <tr class="bg-white dark:bg-black mb-3 p-3">
-    <td class="">
+    <td class="px-3 py-1">
         <?= $this->Html->link(__d('cake_d_c/users', h($user->username)), ['action' => 'view', $user->id],['class' => 'font-bold']) ?> 
     </td>
     <td>
@@ -64,13 +77,13 @@
 </tr>
 <?php endforeach; ?>
 </table>
-<div class="paginator">
-<ul class="pagination">
+<div class="paginator m-3 p-3 bg-slate-200 dark:bg-slate-800 rounded-lg">
+<div class="pagination">
 <?= $this->Paginator->prev('< ' . __d('cake_d_c/users', 'previous')) ?>
 <?= $this->Paginator->numbers() ?>
 <?= $this->Paginator->next(__d('cake_d_c/users', 'next') . ' >') ?>
-</ul>
-<p><?= $this->Paginator->counter() ?></p>
+</div>
+<div><?= $this->Paginator->counter() ?></div>
 </div>
 </div>
 </div>
