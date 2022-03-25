f15998960dfc69fd8539cec9db485907ce3b81fa
Allan Haggett <allan.haggett@gov.bc.ca>
2022-03-25 14:31:12 -0700

    Top 5 pathways followed

--- config/tocopy/SimpleCrudTrait.php
+++ config/tocopy/SimpleCrudTrait.php
@@ -32,17 +32,21 @@ trait SimpleCrudTrait
             'actions' => ['search'],
         ]);
     }
+
     /**
-     * Index method
+     * Index method acts as dashboard for curators where we list the first
+     * page of users, but also list a bunch of statistics and a menu for 
+     * further convenience.
      *
      * @return void
      */
     public function index()
     {
-        $table = $this->loadModel();
-        $alphabetized = $table->find('all')->order(['last_name' => 'asc']);
-        $tableAlias = $table->getAlias();
-        
+        // The initial query to pull out the user list
+        // $table = $this->loadModel();
+        // $alphabetized = $table->find('all')->order(['last_name' => 'asc']);
+        // $tableAlias = $table->getAlias();
+
         // Get top 5 launched activities and a count for total launches
         $au = TableRegistry::getTableLocator()->get('ActivitiesUsers');
         $uselaunches = $au->find('all')->contain('Activities')->order(['activity_id' => 'asc']);;
@@ -79,9 +83,7 @@ trait SimpleCrudTrait
                     // #TODO shouldn't be doing formatting here but when I build an array
                     // the array_unique call below breaks and it took me so long to get to
                     // this point that I'm just rolling with it for now.
-                    $link = '<a href="/activities/view/' . $id . '">' . $val['actname'] . '</a> ';
-                    $link .= '<span class="inline-block px-2 py-1 text-xs bg-white dark:bg-black rounded-lg">';
-                    $link .= $count . ' launches</span><br>';
+                    $link = '<a href="/activities/view/' . $id . '">' . $val['actname'] . '</a> <span class="inline-block px-2 py-1 text-sm bg-white dark:bg-black rounded-lg">' . $count . ' launches</span><br>';
                     array_push($links, $link);
                 }
             }
@@ -91,18 +93,60 @@ trait SimpleCrudTrait
         // let's reduce that down to one line per activity and we're done!
         $top5links = array_unique($links);
 
-        // #TODO pull out top 5 followed pathways as per above, but for this
-        // type too...
-        $pu = TableRegistry::getTableLocator()->get('PathwaysUsers');
-        $pathpins = $pu->find('all');
-        $ppincount = $pathpins->count();
 
-        $this->set($tableAlias, $this->paginate($alphabetized));
-        $this->set('tableAlias', $tableAlias);
-        $this->set('_serialize', [$tableAlias, 'tableAlias']);
-        $this->set('ppincount', $ppincount);
+        // Pull out top 5 followed pathways as per above, with the same caveats that
+        // technique here is ... likely awful
+        $pu = TableRegistry::getTableLocator()->get('PathwaysUsers');
+        $pathfollows = $pu->find('all')->contain('Pathways');
+        $totalfollowcount = $pathfollows->count();
+
+        // setup a few arrays for reducing the massive query results 
+        // down to the top 5
+        $mostfollowed = array();
+        $pathnames = array();
+        $plinks = array();
+        // #TODO This probably isn't a good way of going about this
+        // I'm creating two arrays here and merging them below because
+        // I can't seem to make the count of all activities sort properly
+        // in one go in a multidimensional array (so it includes the 
+        // activity name as well as the ID--needed to make the link), 
+        // so I extract a list that contains the counted and sorted IDs
+        // and then use the 2nd array to extract the names. #inelegantbutworks
+        foreach($pathfollows->toList() as $ul) {
+            array_push($mostfollowed,$ul['pathway_id']);
+            array_push($pathnames,['pathid' => $ul['pathway_id'], 'pathname' => $ul['pathway']['name'], 'pathslug' => $ul['pathway']['slug']]);
+        }
+        $followcounts = array_count_values($mostfollowed);
+        arsort($followcounts);
+        // So far we've counted and sorted the entire list, so now we 
+        // just slice off the top 5. It is probably more efficient to do 
+        // this earlier 
+        $top5f = array_slice($followcounts,0,5,true);
+        foreach($top5f as $id => $count) {
+            // Needing to loop through the entire list of activity launches
+            // for each of the top 5 links to extract the name is probably
+            // a dumb way of doing this, but #inelegantbutworks #refactor
+            foreach($pathnames as $key => $val) {
+                if($val['pathid'] == $id) {
+                    // #TODO shouldn't be doing formatting here but when I build an array
+                    // the array_unique call below breaks and it took me so long to get to
+                    // this point that I'm just rolling with it for now.
+                    $plink = '<a href="/pathways/' . $val['pathslug'] . '">' . $val['pathname'] . '</a> <span class="inline-block px-2 py-1 text-sm bg-white dark:bg-black rounded-lg">' . $count . ' follows</span><br>';
+                    array_push($plinks, $plink);
+                }
+            }
+        }
+        // We have a list of the top 5, but each link it repeated for each launch
+        // i.e. if it's launched 11 times, it's listed 11 times
+        // let's reduce that down to one line per activity and we're done!
+        $top5follows = array_unique($plinks);
+
+        $this->set('totalfollowcount', $totalfollowcount);
         $this->set('launchcount', $launchcount);
+        $this->set('top5links', $top5links);
+        $this->set('top5follows', $top5follows);
     }
+
 
     /**
      * Search method
@@ -156,8 +200,8 @@ trait SimpleCrudTrait
         $this->set('tableAlias', $tableAlias);
         $this->set('_serialize', [$tableAlias, 'tableAlias']);
     }
-    
-   /**
+
+    /**
      * API method
      *
      * @param string|null $id User id.
@@ -190,8 +234,6 @@ trait SimpleCrudTrait
 
     }
 
-
-
     /**
      * Add method
      *
@@ -199,15 +241,11 @@ trait SimpleCrudTrait
      */
     public function add()
     {
-        $mins = TableRegistry::getTableLocator()->get('Ministries');
-        $ministries = $mins->find('list');
-        
         $table = $this->loadModel();
         $tableAlias = $table->getAlias();
         $entity = $table->newEmptyEntity();
         $this->set($tableAlias, $entity);
         $this->set('tableAlias', $tableAlias);
-        $this->set('ministries', $ministries);
         $this->set('_serialize', [$tableAlias, 'tableAlias']);
         if (!$this->getRequest()->is('post')) {
             return;
@@ -237,7 +275,7 @@ trait SimpleCrudTrait
         $table = $this->loadModel();
         $tableAlias = $table->getAlias();
         $entity = $table->get($id, [
-            'contain' => [],
+            'contain' => ['Ministries'],
         ]);
         $this->set($tableAlias, $entity);
         $this->set('tableAlias', $tableAlias);
--- templates/plugin/CakeDC/Users/Users/index.php
+++ templates/plugin/CakeDC/Users/Users/index.php
@@ -12,79 +12,62 @@
 <div class="p-6 dark:text-white">
 
 <div class="text-3xl mb-3">
-    <span class="inline-block p-3 rounded-full bg-slate-300 dark:bg-black"><?= $ppincount ?></span> Pathway Pins 
+    <span class="inline-block p-3 rounded-full bg-slate-300 dark:bg-black"><?= $totalfollowcount ?></span> Pathway Pins 
     <span class="inline-block p-3 rounded-full bg-slate-300 dark:bg-black"><?= $launchcount ?></span> Activity Launches
 </div>
 
+<form method="get" action="/users/search" class="pt-3">
+<label class="">Search for a user:
+<input class="px-3 py-2 m-0 dark:text-white dark:bg-slate-900 rounded-l-lg" 
+        type="search" 
+        placeholder="first or last name ..." 
+        aria-label="User Search" 
+        name="q"></label><button class="px-3 py-2 m-0 bg-slate-300 dark:text-white dark:bg-slate-900 dark:hover:bg-slate-800 rounded-r-lg" type="submit">User Search</button>
+</form>
+
 <div class="mt-6 mb-2">
+    View: 
     <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/reports/index">Reports</a>
     <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/activity-types">Activity Types</a>
     <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/ministries">Ministries</a>
 </div>
 <div class="mb-4">
-    <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/categories/add">New Category</a>
-    <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/topics/add">New Topic</a>
-    <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/pathways/add">New Pathway</a> 
-    <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/activities/add">New Activity</a>
+    Create new: 
+    <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/categories/add">Category</a>
+    <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/topics/add">Topic</a>
+    <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/pathways/add">Pathway</a> 
+    <a class="inline-block p-3 mr-1 bg-slate-200 dark:bg-sky-700 hover:no-underline hover:bg-sky-800 rounded-lg" href="/activities/add">Activity</a>
     <!-- <a class="inline-block p-3 my-3 bg-slate-200 dark:bg-sky-700" href="/activities/addtostep">Add Activity to Step</a> -->
 </div>
+
+
+
+<h2 class="mt-4 text-2xl">Top 5 Followed Pathways</h2>
+<div class="p-3 bg-slate-200 dark:bg-slate-900 rounded-lg">
+<ol class="pl-10">
+<?php 
+// these links are templated/built within the controller for noted reasons there
+
+?>
+<?php foreach($top5follows as $link): ?>
+    <li class="p-2 list-decimal text-xl"><?= $link ?></li>
+<?php endforeach ?>
+</ol>
+</div>
+
 
 <h2 class="mt-4 text-2xl">Top 5 Launched Activities</h2>
 <div class="p-3 bg-slate-200 dark:bg-slate-900 rounded-lg">
 <ol class="pl-10">
 <?php 
 // these links are templated/built within the controller for noted reasons there
-$count = 4;
+
 ?>
 <?php foreach($top5links as $link): ?>
     <li class="p-2 list-decimal text-xl"><?= $link ?></li>
-    <?php $count-- ?>
 <?php endforeach ?>
 </ol>
 </div>
 
 
-<form method="get" action="/users/search" class="pt-6">
-<label class="">Search for a user:
-<input class="px-3 py-2 m-0 dark:text-white dark:bg-slate-900 rounded-l-lg" 
-        type="search" 
-        placeholder="first or last name ..." 
-        aria-label="Search" 
-        name="q"></label><button class="px-3 py-2 m-0 bg-slate-300 dark:text-white dark:bg-slate-900 dark:hover:bg-slate-800 rounded-r-lg" type="submit">Search</button>
-</form>
-<div class="mt-1 p-3 bg-white dark:bg-slate-900 rounded-lg">
-<table class="w-full">
-    <tr class="bg-slate-200 dark:bg-slate-800">
-        <th>User name</th>
-        <th>First name</th>
-        <th>Last name</th>
-        <th>Email</th>
-    </tr>  
-<?php foreach (${$tableAlias} as $user) : ?>
-<tr class="bg-white dark:bg-black mb-3 p-3">
-    <td class="px-3 py-1">
-        <?= $this->Html->link(__d('cake_d_c/users', h($user->username)), ['action' => 'view', $user->id],['class' => 'font-bold']) ?> 
-    </td>
-    <td>
-        <?= h($user->first_name) ?> 
-    </td>
-    <td>
-        <?= h($user->last_name) ?>
-    </td>
-    <td>
-        <?= h($user->email) ?>
-    </td>
-    </div>
-</tr>
-<?php endforeach; ?>
-</table>
-<div class="paginator m-3 p-3 bg-slate-200 dark:bg-slate-800 rounded-lg">
-<div class="pagination">
-<?= $this->Paginator->prev('< ' . __d('cake_d_c/users', 'previous')) ?>
-<?= $this->Paginator->numbers() ?>
-<?= $this->Paginator->next(__d('cake_d_c/users', 'next') . ' >') ?>
-</div>
-<div><?= $this->Paginator->counter() ?></div>
-</div>
-</div>
 </div>
