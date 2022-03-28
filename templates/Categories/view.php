44a5e4f0b57613e74fdd274aa0bac3e8f35c2c75
Allan Haggett <allan.haggett@gov.bc.ca>
2022-03-28 10:43:51 -0700

    Formatting

--- templates/Categories/view.php
+++ templates/Categories/view.php
@@ -23,39 +23,21 @@ $this->assign('title', $pagetitle);
 	<?= h($category->name) ?>
 </nav>
 
-<h1 class="text-4xl my-3">
-	<?= h($category->name) ?>
-</h1>
-
-<div class="p-3 bg-slate-200 dark:bg-[#002850] rounded-lg">
-<?= $this->Text->autoParagraph(h($category->description)); ?>
-</div>
-
 <?php if($role == 'curator' || $role == 'superuser'): ?>
-<div class="hidden">
-		<?= $this->Form->create(null,['url' => ['controller' => 'Topics', 'action' => 'add']]) ?>
-		<fieldset>
-			<?php
-				echo $this->Form->control('name', ['class' => 'form-control']);
-				?>
-				<label for="description">Description</label>
-				<?php
-				echo $this->Form->textarea('description', ['class' => 'form-control', 'id' => 'description']);
-				?>
-				<?php
-				//echo $this->Form->control('image_path');
-				///echo $this->Form->control('color');
-				//echo $this->Form->control('featured');
-				echo $this->Form->hidden('user_id', ['value' => $uid]);
-				echo $this->Form->hidden('categories.0.id', ['value' => $category->id]);
-			?>
-		</fieldset>
-		<?= $this->Form->button(__('Add Topic'), ['class' => 'btn btn-primary mt-2']) ?>
-		<?= $this->Form->end() ?>
+<div class="mt-3 float-right">
+<?= $this->Html->link(__('Edit Category'), ['action' => 'edit', $category->id], ['class' => 'inline-block px-4 py-2 text-md bg-sky-700 dark:text-white dark:bg-sky-700 dark:focus:text-white dark:hover:text-white dark:focus:bg-slate-900 dark:hover:bg-slate-900 hover:text-slate-900 focus:text-slate-900 hover:bg-slate-200 focus:bg-slate-200 focus:outline-none focus:shadow-outline hover:no-underline rounded-lg']) ?>
+<?= $this->Html->link(__('Add Topic'), ['controller' => 'Topics', 'action' => 'add'], ['class' => 'inline-block px-4 py-2 text-md bg-sky-700 dark:text-white dark:bg-sky-700 dark:focus:text-white dark:hover:text-white dark:focus:bg-slate-900 dark:hover:bg-slate-900 hover:text-slate-900 focus:text-slate-900 hover:bg-slate-200 focus:bg-slate-200 focus:outline-none focus:shadow-outline hover:no-underline rounded-lg']) ?>
 </div>
 <?php endif;  // curator or admin? ?>
 
 
+<h1 class="text-4xl my-3">
+	<?= h($category->name) ?>
+</h1>
+
+<div class="p-3 bg-slate-200 dark:bg-slate-800 rounded-lg">
+<?= $this->Text->autoParagraph(h($category->description)); ?>
+</div>
 
 <?php if (!empty($category->topics)) : ?>
 <?php foreach ($category->topics as $topic) : ?>
@@ -65,10 +47,14 @@ $this->assign('title', $pagetitle);
 <h2 class="text-3xl">
 	<!-- topic_id: <?= $topic->id ?> --> 
 	<?= $this->Html->link(h($topic->name), ['controller' => 'Topics', 'action' => 'view', $topic->id]) ?>
-	
 </h2>
-<div class="py-2 mb-2"><?= $topic->description ?></div>
 
+<div class="p-3 mb-2 bg-slate-300 dark:bg-slate-800">
+	<?= $topic->description ?>
+</div>
+<a href="/topics/view/<?= $topic->id ?>" class="inline-block my-2 p-3 bg-sky-600 hover:bg-sky-800 rounded-lg text-white text-xl hover:no-underline">
+	Launch Topic
+</a>
 
 </div>
 <?php endif; // is published ?>
