25b0ab5f0d3fb007ff2eb6b50f899e51e33c6384
Allan Haggett <allan.haggett@gov.bc.ca>
2022-03-30 13:06:04 -0700

    Add launch button

--- templates/ActivitiesUsers/launches.php
+++ templates/ActivitiesUsers/launches.php
@@ -30,6 +30,17 @@ $uid = $this->Identity->get('id');
 	<?php endforeach ?>
 
 </div>
+<a target="_blank" 
+	rel="noopener" 
+	data-toggle="tooltip" data-placement="bottom" title="Launch this activity"
+	href="/activities-users/launch?activity_id=<?= $a['id'] ?>" 
+	class="inline-block my-2 p-3 bg-sky-700 rounded-lg text-white text-xl hover:no-underline">
+		Launch Activity 
+		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="inline bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+			<path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
+			<path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
+		</svg>
+</a>
 
 </div>
 <?php endforeach ?>
