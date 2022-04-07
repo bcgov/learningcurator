424a58b2673c79bcd2cfdd8182dc31787ab63145
Allan Haggett <allan.haggett@gov.bc.ca>
2022-04-06 21:23:51 -0700

    Formatting

--- templates/Reports/reports.php
+++ templates/Reports/reports.php
@@ -18,44 +18,52 @@ if ($this->Identity->isLoggedIn()) {
 	
 	<?php foreach ($reports as $report) : ?>
 	<div class="mb-3 p-3 bg-white dark:bg-slate-800 rounded-lg">
-	<?= $this->Form->postLink(__('Delete'), ['controller' => 'Reports', 'action' => 'delete', $report->id], ['confirm' => __('Are you sure you want to delete this report?', $report->id), 'class' => 'float-right btn btn-primary']) ?>
-		<?= h($report->created) ?>
-		<div><strong><a href="/activities/view/<?= $report->activity->id ?>"><?= $report->activity->name ?></a></strong></div>
-		<blockquote class="p-3 my-1 bg-light">
+		<?= $this->Form->postLink(__('Remove Issue'), ['controller' => 'Reports', 'action' => 'delete', $report->id], ['confirm' => __('Are you sure you want to delete this report?', $report->id), 'class' => 'float-right inline-block p-3 mb-1 ml-3 bg-slate-200 dark:bg-sky-700 dark:hover:bg-sky-800 dark:text-white hover:no-underline rounded-lg']) ?>
+		
+		<div class="text-sm">
+			You filed issue report #<?= $report->id ?> for the following activity on 
+			<?= $this->Time->format($report->created,\IntlDateFormatter::MEDIUM,null,'GMT-8') ?>:
+		</div>
+		<div class="my-6 text-xl"><a href="/activities/view/<?= $report->activity->id ?>"><?= $report->activity->name ?></a></div>
+		<div class="text-sm italic">You said:</div>
+		<blockquote class="p-3 my-1 bg-slate-100 dark:bg-slate-900 rounded-lg">
 			<?= h($report->issue) ?>
 		</blockquote>
 		<?php if(!empty($report->response)): ?>
-			<div class="alert alert-success"><?= h($report->response) ?></div>
+			<div class="p-3 my-1 bg-slate-100 dark:bg-slate-900 rounded-lg"><?= h($report->response) ?></div>
 		<?php else: ?>
-			<div class="alert alert-primary">No response yet.</div>
+			<div class="text-sm italic">Curator Responds:</div>
+			<div class="p-3 my-1 bg-slate-100 dark:bg-slate-900 rounded-lg">There is no response from a Curator yet. Please be patient.</div>
 		<?php endif ?>
+
+
 		<?php if($role == 'curator' || $role == 'superuser'): ?>
-		<a href="#curatorresponse<?= $report->id ?>" 
-			style="color:#333;" 
-			class="btn btn-light" 
-			data-toggle="collapse" 
-			title="Respond to this report" 
-			data-target="#curatorresponse<?= $report->id ?>" 
-			aria-expanded="false" 
-			aria-controls="curatorresponse<?= $report->id ?>">
+
+
+
+
+			<div x-data="{ open: false }">
+			<button @click="open = ! open" class="inline-block p-3 mb-1 ml-3 bg-slate-200 dark:bg-sky-700 dark:hover:bg-sky-800 dark:text-white hover:no-underline rounded-lg">
 				Respond
-		</a>	
-		<div class="collapse" id="curatorresponse<?= $report->id ?>">
-		<?php if($role == 'superuser'): ?>
-		<?= $this->Form->postLink(__('Delete'), ['controller' => 'Reports', 'action' => 'delete', $report->id], ['confirm' => __('Are you sure you want to delete this report?', $report->id), 'class' => 'float-right btn btn-primary']) ?>
-		<?php endif ?>
-		<?= $this->Form->create(null,['url' => ['controller' => 'reports','action' => 'edit', $report->id]]) ?>
-		<fieldset>
-		<legend><?= __('Respond') ?></legend>
-		<?php
-		echo $this->Form->hidden('id', ['value' => $report->id]);
-		echo $this->Form->hidden('curator_id', ['value' =>  $this->Identity->get('id')]);
-		echo $this->Form->textarea('response',['class' => 'form-control', 'placeholder' => 'Type here ...']);
-		?>
-		</fieldset>
-		<input type="submit" class="btn btn-primary" value="Submit Response">
-		<?= $this->Form->end() ?>
-		</div> <!-- curatorresponse -->
+			</button>
+			<div xcloak x-show="open" class="p-3 my-3 rounded-lg bg-white dark:bg-slate-800 dark:text-white">
+
+			<?= $this->Form->create(null,['url' => ['controller' => 'reports','action' => 'edit', $report->id]]) ?>
+			<fieldset>
+			<legend><?= __('Respond') ?></legend>
+			<?php
+			echo $this->Form->hidden('id', ['value' => $report->id]);
+			echo $this->Form->hidden('curator_id', ['value' =>  $this->Identity->get('id')]);
+			echo $this->Form->textarea('response',['class' => 'block w-full px-3 py-1 m-0 dark:text-white dark:bg-slate-900 rounded-lg', 'placeholder' => 'Type here ...']);
+			?>
+			</fieldset>
+			<input type="submit" class="btn btn-primary" value="Submit Response">
+			<?= $this->Form->end() ?>
+
+			</div>
+		</div>
+
+
 		<?php endif ?>
 
 	</div>
