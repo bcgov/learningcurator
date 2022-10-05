f3d1c5263a31ca6b79b9da35c7409df4bbf1019a
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-10-05 14:31:36 -0700

    add nav and button green

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  content: ['./templates/**/*.php'],
-  theme: {
-    extend: {
-      fontFamily: {
-        'BCSans': ['BCSans', 'sans-serif'] 
-      },
+    content: ['./templates/**/*.php'],
+    theme: {
+        extend: {
+            fontFamily: {
+                'BCSans': ['BCSans', 'sans-serif']
+            },
+            colors: {
+                'sage-green': '#DFE9D7',
+                'sage-green-dark': '#7B8F6A',
+            },
+        },
     },
-  },
-  //darkMode: 'class',
-  plugins: [],
+    //darkMode: 'class',
+    plugins: [],
 }
