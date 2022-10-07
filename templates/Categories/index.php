0d9d5b7a444c84b3824cf57fdc5e51fa9df2b739
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-10-07 11:36:50 -0700

    add styling to category boxes

--- templates/Categories/index.php
+++ templates/Categories/index.php
@@ -18,7 +18,7 @@ if ($this->Identity->isLoggedIn()) {
     <div class="bg-sky-700/90 h-44 w-72 drop-shadow-lg p-4 flex"><span class="text-white text-3xl font-bold m-auto tracking-wide">Categories</span></div>
 </header>
 
-<div class="p-8 text-xl">
+<div class="p-8 text-lg">
     <div class="max-w-prose">
         <h2 class="mb-3 text-2xl text-darkblue font-semibold">TBD</h2>
 
@@ -27,36 +27,30 @@ if ($this->Identity->isLoggedIn()) {
     </div>
     <div class="grid grid-cols-1 lg:grid-cols-7 lg:gap-4">
         <div class="lg:col-span-5 max-w-prose order-last lg:order-first">
-
+            <!-- TODO sort alphabetically? -->
             <?php foreach ($categories as $category) : ?>
 
-                <div class="rounded-lg shadow-lg border">
-                    <div class="my-2 h-40 ">
-                        <?php if (empty($category->featured)) : ?>
-                            <span class="inline-block py-0 px-2 bg-yellow-600 text-white text-xs" title="Edit to set to publish">DRAFT</span>
-                        <?php endif ?>
+                <div class="rounded-md shadow-lg border-2 border-sky-700 mb-4">
 
-                        <h1 class="text-3xl">
+                    <?php if (empty($category->featured)) : ?>
+                        <span class="inline-block py-0 px-2 my-2 bg-yellow-600 text-white text-xs" title="Edit to set to publish">DRAFT</span>
+                    <?php endif ?>
 
-                            <a class="block mt-6 p-3 bg-white/80 hover:bg-white text-black shadow-lg hover:no-underline" href="/category/<?= $category->id ?>/<?= h($category->slug) ?>">
-                                <?= h($category->name) ?>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="inline bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-                                    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-                                </svg>
-                            </a>
+                    <h1 class="text-2xl bg-sky-700 text-white p-3">
+                        <?= h($category->name) ?>
+                    </h1>
+                    <div class="p-3">
+                        <p class="mb-0"><?= h($category->description) ?></p>
 
-                        </h1>
-
-                        <!-- <div class="my-3">
-    <?= h($category->description) ?>
-</div> -->
-
-                    </div> <!-- overlay color -->
+                        <a class="inline-block p-3 mt-4 bg-sky-700 text-white text-xl hover:no-underline rounded-lg" href="/category/<?= $category->id ?>/<?= h($category->slug) ?>">
+                            Explore <span class="font-bold"><?= h($category->name) ?></span>
+                        </a>
+                    </div>
                 </div> <!-- formatting container -->
             <?php endforeach; ?>
         </div>
         <!-- sort options appear to the side on larger screens, but on top on smaller screens -->
-        <div class="my-3 lg:col-span-2">
+        <div class="my-3 ml-8 lg:col-span-2">
             Sort options
         </div>
 
