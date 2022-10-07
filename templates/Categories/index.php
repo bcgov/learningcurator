e3bf2657ff1454e61523079cccd90d146b2d8b60
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-10-07 14:42:21 -0700

    change span to h1 in banner

--- templates/Categories/index.php
+++ templates/Categories/index.php
@@ -15,7 +15,7 @@ if ($this->Identity->isLoggedIn()) {
 }
 ?>
 <header class="w-full h-52 bg-cover bg-[center_top_65%] pb-8 px-8" style="background-image: url(/img/categories/Path_in_Sumallo_Grove-compressed.jpg);">
-    <div class="bg-sky-700/90 h-44 w-72 drop-shadow-lg p-4 flex"><span class="text-white text-3xl font-bold m-auto tracking-wide">Categories</span></div>
+    <div class="bg-sky-700/90 h-44 w-72 drop-shadow-lg p-4 flex"><h1 class="text-white text-3xl font-bold m-auto tracking-wide">Categories</h1></div>
 </header>
 
 <div class="p-8 text-lg">
--- templates/Categories/view.php
+++ templates/Categories/view.php
@@ -17,7 +17,7 @@ $pagetitle = $category->name . '';
 $this->assign('title', $pagetitle);
 ?>
 <header class="w-full h-52 bg-cover bg-[center_top_65%] pb-8 px-8" style="background-image: url(/img/categories/Path_in_Sumallo_Grove-compressed.jpg);">
-    <div class="bg-sky-700/90 h-44 w-72 drop-shadow-lg p-4 flex"><span class="text-white text-3xl font-bold m-auto tracking-wide">Categories</span></div>
+    <div class="bg-sky-700/90 h-44 w-72 drop-shadow-lg p-4 flex"><h1 class="text-white text-3xl font-bold m-auto tracking-wide">Categories</h1></div>
 </header>
 <?php if ($role == 'curator' || $role == 'superuser') : ?>
         <div class="p-4 float-right">
@@ -27,20 +27,23 @@ $this->assign('title', $pagetitle);
     <?php endif;  // curator or admin? 
     ?>
 <div class="p-8 pt-4 w-full">
-
-
-
-
     <nav class="mb-3 p-2 text-sagedark text-sm" aria-label="breadcrumb">
         <?= $this->Html->link(__('Categories'), ['controller' => 'Categories', 'action' => 'index'], ['class' => '']) ?> /
         <?= h($category->name) ?>
     </nav>
 
-    <div class="my-3 p-3 bg-white/80 rounded-lg">
-        <h1 class="text-4xl">
+    
+    <div class="max-w-prose">
+        <h2 class="mb-3 text-2xl text-darkblue font-semibold">TBD</h2>
+
+        <p class="mb-3">
+            Category intro text here</p>
+    </div>
+    <div class="my-3">
+        <h1 class="text-4xl text-darkblue">
             <?= h($category->name) ?>
         </h1>
-        <div class="p-3 mb-2 text-xl bg-white rounded-lg">
+        <div class="text-xl">
             <?= $this->Text->autoParagraph(h($category->description)); ?>
         </div>
     </div>
