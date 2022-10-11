daeb3eacb80577d819bb9b7bf96d3ce586794e66
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-10-11 12:01:01 -0700

    remove background

--- templates/Topics/view.php
+++ templates/Topics/view.php
@@ -13,11 +13,12 @@ if ($this->Identity->isLoggedIn()) {
 }
 ?>
 <header class="w-full h-52 bg-cover bg-[center_top_65%] pb-8 px-8" style="background-image: url(/img/categories/Path_in_Sumallo_Grove-compressed.jpg);">
-    <div class="bg-darkblue/90 h-44 w-72 drop-shadow-lg p-4 flex">
-        <h1 class="text-white text-3xl font-bold m-auto tracking-wide">Pathways</h1>
+    <div class="bg-sky-700/90 h-44 w-72 drop-shadow-lg p-4 flex">
+        <h1 class="text-white text-3xl font-bold m-auto tracking-wide">Categories</h1>
     </div>
 </header>
-<div class="p-6 w-full bg-top bg-no-repeat md:bg-fixed min-h-screen rounded-tr-xl" style="background-image: url('<?= h($topic->categories[0]->image_path) ?>')">
+<!-- TODO should this be Pathways or Categories label here? -->
+<div class="p-6 w-full min-h-screen rounded-tr-xl">
 
 
 <nav class="mb-3 p-3 bg-white dark:bg-slate-900 rounded-lg" aria-label="breadcrumb">
