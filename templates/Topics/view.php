51af261ab31fe00ab7b79c1d58e4b3d4776bdced
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-10-11 15:42:42 -0700

    padding update

--- templates/Topics/view.php
+++ templates/Topics/view.php
@@ -30,7 +30,7 @@ if ($this->Identity->isLoggedIn()) {
     <?php if ($role == 'curator' || $role == 'superuser') : ?>
         <div class="p-4 float-right">
             <?= $this->Html->link(__('Edit Topic'), ['action' => 'edit', $topic->id], ['class' => 'inline-block px-4 py-2 text-white text-md bg-slate-700 hover:text-slate-900 focus:text-slate-900 hover:bg-slate-200 focus:bg-slate-200 focus:outline-none focus:shadow-outline hover:no-underline rounded-lg']) ?>
-        
+
         </div>
 
 
@@ -51,7 +51,7 @@ if ($this->Identity->isLoggedIn()) {
         <?php foreach ($topic->pathways as $pathway) : ?>
             <?php if ($pathway->status_id == 2) : ?>
 
-                <div class="p-3 mb-3 mt-5 bg-bluegreen text-white w-full point-left">
+                <div class="p-3 mb-3 mt-8 bg-bluegreen text-white w-full point-left">
                     <h3 class="text-2xl flex items-center justify-between">
                         <a href="/<?= h($topic->categories[0]->slug) ?>/<?= $topic->slug ?>/pathway/<?= h($pathway->slug) ?>">
                             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-signpost-2 inline-block mx-3" viewBox="0 0 16 16">
@@ -63,39 +63,37 @@ if ($this->Identity->isLoggedIn()) {
                     <!-- TODO add code to pull in steps/activities -->
                 </div>
                 <div class="pl-10 text-lg">
-                    <?= h($pathway->description) ?>
+                    <p><?= h($pathway->description) ?></p>
+
+                    <a href="/<?= h($topic->categories[0]->slug) ?>/<?= $topic->slug ?>/pathway/<?= h($pathway->slug) ?>" class="text-sky-700 underline">
+                        View the <strong><?= h($pathway->name) ?></strong> pathway
+                    </a>
                 </div>
-                <a href="/<?= h($topic->categories[0]->slug) ?>/<?= $topic->slug ?>/pathway/<?= h($pathway->slug) ?>" class="inline-block my-2 p-3 bg-sky-700 hover:bg-sky-800 rounded-lg text-white text-xl hover:no-underline">
-                    View Pathway
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="inline bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-                        <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-                    </svg>
-                </a>
+
+
+
+            <?php else : ?>
+                <?php if ($role == 'curator' || $role == 'superuser') : ?>
+                    <div class="p-3 my-3 bg-white/80 rounded-lg shadow-sm dark:bg-slate-900/80 dark:text-white">
+                        <div class="badge badge-warning">DRAFT</div>
+                        <h2 class="text-2xl">
+                            <a href="/<?= h($topic->categories[0]->slug) ?>/<?= $topic->slug ?>/pathway/<?= h($pathway->slug) ?>">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="inline bi bi-compass" viewBox="0 0 16 16">
+                                    <path d="M8 16.016a7.5 7.5 0 0 0 1.962-14.74A1 1 0 0 0 9 0H7a1 1 0 0 0-.962 1.276A7.5 7.5 0 0 0 8 16.016zm6.5-7.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z" />
+                                    <path d="m6.94 7.44 4.95-2.83-2.83 4.95-4.949 2.83 2.828-4.95z" />
+                                </svg>
+                                <?= h($pathway->name) ?>
+                            </a>
+                        </h2>
+                        <div class="p-4 text-lg bg-slate-100/80 dark:bg-slate-800 rounded-lg">
+                            <?= h($pathway->description) ?>
+                        </div>
+                        <a href="/<?= h($topic->categories[0]->slug) ?>/<?= $topic->slug ?>/pathway/<?= h($pathway->slug) ?>" class="inline-block my-2 p-3 bg-sky-700 hover:bg-sky-800 rounded-lg text-white text-xl hover:no-underline">
+                            View Pathway
+                        </a>
+                    </div>
+                <?php endif ?>
+            <?php endif ?>
+        <?php endforeach ?>
     </div>
-
-
-<?php else : ?>
-    <?php if ($role == 'curator' || $role == 'superuser') : ?>
-        <div class="p-3 my-3 bg-white/80 rounded-lg shadow-sm dark:bg-slate-900/80 dark:text-white">
-            <div class="badge badge-warning">DRAFT</div>
-            <h2 class="text-2xl">
-                <a href="/<?= h($topic->categories[0]->slug) ?>/<?= $topic->slug ?>/pathway/<?= h($pathway->slug) ?>">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="inline bi bi-compass" viewBox="0 0 16 16">
-                        <path d="M8 16.016a7.5 7.5 0 0 0 1.962-14.74A1 1 0 0 0 9 0H7a1 1 0 0 0-.962 1.276A7.5 7.5 0 0 0 8 16.016zm6.5-7.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z" />
-                        <path d="m6.94 7.44 4.95-2.83-2.83 4.95-4.949 2.83 2.828-4.95z" />
-                    </svg>
-                    <?= h($pathway->name) ?>
-                </a>
-            </h2>
-            <div class="p-4 text-lg bg-slate-100/80 dark:bg-slate-800 rounded-lg">
-                <?= h($pathway->description) ?>
-            </div>
-            <a href="/<?= h($topic->categories[0]->slug) ?>/<?= $topic->slug ?>/pathway/<?= h($pathway->slug) ?>" class="inline-block my-2 p-3 bg-sky-700 hover:bg-sky-800 rounded-lg text-white text-xl hover:no-underline">
-                View Pathway
-            </a>
-        </div>
-    <?php endif ?>
-<?php endif ?>
-<?php endforeach ?>
-</div>
 </div>
