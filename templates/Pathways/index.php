a01fb85b9e989c440689b6ddf048a638f86c1674
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-10-11 16:11:51 -0700

    add sort options

--- templates/Pathways/index.php
+++ templates/Pathways/index.php
@@ -31,42 +31,81 @@ if ($this->Identity->isLoggedIn()) {
 
 
             <?php foreach ($pathways as $pathway) : ?>
-                <div class="p-6 mb-3 w-full bg-center bg-no-repeat rounded-lg" style="background-image: url('<?= h($pathway->topic->categories[0]->image_path) ?>')">
-                    <div class="p-3 text-xl bg-slate-100/80 dark:bg-slate-900/80 rounded-lg">
+                <div class="p-3 mb-3 mt-8 bg-bluegreen text-white w-full point-left">
+                    <h3 class="text-2xl flex items-center justify-between">
+                        <a href="/<?= h($pathway->topic->categories[0]->slug) ?>/<?= h($pathway->topic->slug) ?>/pathway/<?= h($pathway->slug) ?>">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-signpost-2 inline-block mx-3" viewBox="0 0 16 16">
+                                <path d="M7 1.414V2H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h5v1H2.5a1 1 0 0 0-.8.4L.725 8.7a.5.5 0 0 0 0 .6l.975 1.3a1 1 0 0 0 .8.4H7v5h2v-5h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H9V6h4.5a1 1 0 0 0 .8-.4l.975-1.3a.5.5 0 0 0 0-.6L14.3 2.4a1 1 0 0 0-.8-.4H9v-.586a1 1 0 0 0-2 0zM13.5 3l.75 1-.75 1H2V3h11.5zm.5 5v2H2.5l-.75-1 .75-1H14z" />
+                            </svg><?= h($pathway->name) ?>
+                        </a><span class="text-sm">8 steps | 23 activities</span>
+                    </h3>
+                </div>
+                <!-- TODO add code to pull in steps/activities -->
 
-                        <h2 class="mb-2 text-2xl">
-                            <a href="/<?= h($pathway->topic->categories[0]->slug) ?>/<?= h($pathway->topic->slug) ?>/pathway/<?= h($pathway->slug) ?>" class="font-weight-bold">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="inline bi bi-compass" viewBox="0 0 16 16">
-                                    <path d="M8 16.016a7.5 7.5 0 0 0 1.962-14.74A1 1 0 0 0 9 0H7a1 1 0 0 0-.962 1.276A7.5 7.5 0 0 0 8 16.016zm6.5-7.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z" />
-                                    <path d="m6.94 7.44 4.95-2.83-2.83 4.95-4.949 2.83 2.828-4.95z" />
-                                </svg>
-                                <?= h($pathway->name) ?>
-                            </a>
-                        </h2>
-                        <div class="p-3 bg-white dark:bg-slate-800 rounded-lg">
-                            <?= h($pathway->description) ?>
-                        </div>
-                        <a href="/<?= h($pathway->topic->categories[0]->slug) ?>/<?= h($pathway->topic->slug) ?>/pathway/<?= h($pathway->slug) ?>" class="inline-block my-2 p-3 bg-sky-700 hover:bg-sky-800 rounded-lg text-white text-xl hover:no-underline">
-                            View Pathway
-                        </a>
-                        <div class="p-3 bg-white dark:bg-slate-700 rounded-lg">
-                            <?php
-                            $stat = 'badge-light';
-                            if ($pathway->status->name == 'Draft') $stat = 'badge-warning';
-                            ?>
-                            <?php if ($pathway->featured == 1) : ?>
-                                <span class="badge badge-success">Featured</span>
-                            <?php endif ?>
-                            <span class="badge <?= $stat ?>"><?= $pathway->status->name ?></span> in
-                            <?php $topiclink = $pathway->topic->categories[0]->name . ', ' . $pathway->topic->name ?>
-                            <a href="/category/<?= $pathway->topic->categories[0]->id ?>/<?= $pathway->topic->categories[0]->slug ?>/topic/<?= $pathway->topic->id ?>/<?= $pathway->topic->slug ?>">
-                                <?= $topiclink ?>
-                            </a>
+                <div class="pl-10 text-lg">
+                    <p><?= h($pathway->description) ?></p>
 
-                        </div>
-                    </div>
+                    <p> <a href="/<?= h($pathway->topic->categories[0]->slug) ?>/<?= h($pathway->topic->slug) ?>/pathway/<?= h($pathway->slug) ?>" class="text-sky-700 underline">
+                            View the <strong><?= h($pathway->name) ?></strong> pathway
+                    </p>
+                </div>
+                <div class="p-3 bg-white dark:bg-slate-700 rounded-lg">
+                    <?php
+                    $stat = 'badge-light';
+                    if ($pathway->status->name == 'Draft') $stat = 'badge-warning';
+                    ?>
+                    <?php if ($pathway->featured == 1) : ?>
+                        <span class="badge badge-success">Featured</span>
+                    <?php endif ?>
+                    <span class="badge <?= $stat ?>"><?= $pathway->status->name ?></span> in
+                    <?php $topiclink = $pathway->topic->categories[0]->name . ', ' . $pathway->topic->name ?>
+                    <a href="/category/<?= $pathway->topic->categories[0]->id ?>/<?= $pathway->topic->categories[0]->slug ?>/topic/<?= $pathway->topic->id ?>/<?= $pathway->topic->slug ?>">
+                        <?= $topiclink ?>
+                    </a>
+
                 </div>
             <?php endforeach; ?>
-
-
         </div>
+
+        <!-- sort options appear to the side on larger screens, but on top on smaller screens -->
+        <div class="lg:mt-8 lg:basis-1/5">
+            <div class="flex justify-end lg:justify-start gap-4">
+                <!-- TODO add working sort and filter options -->
+                <a href="" class="hover:text-sky-700">
+                    <div class="flex flex-col justify items-center gap-1">
+
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-view-stacked" viewBox="0 0 16 16">
+                            <path d="M3 0h10a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3zm0 8h10a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1H3z" />
+                        </svg>
+                        <p class="text-xs text-center">List View</p>
+
+                    </div>
+                </a>
+                <a href="" class="hover:text-sky-700">
+                    <div class="flex flex-col justify items-center gap-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-grid" viewBox="0 0 16 16">
+                            <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z" />
+                        </svg>
+                        <p class="text-xs text-center">Grid View</p>
+                    </div>
+                </a>
+                <a href="" class="hover:text-sky-700">
+                    <div class="flex flex-col justify items-center gap-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-funnel" viewBox="0 0 16 16">
+                            <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z" />
+                        </svg>
+                        <p class="text-xs text-center">Filter</p>
+                    </div>
+                </a>
+                <a href="" class="hover:text-sky-700">
+                    <div class="flex flex-col justify items-center gap-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-sort-down" viewBox="0 0 16 16">
+                            <path d="M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293V2.5zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z" />
+                        </svg>
+                        <p class="text-xs text-center">Sort</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
