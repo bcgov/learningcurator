a58069dfc1f0ca04945838cb75be8b53230932a8
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-10-12 09:45:37 -0700

    style badges and tags

--- templates/Pathways/index.php
+++ templates/Pathways/index.php
@@ -46,28 +46,33 @@ if ($this->Identity->isLoggedIn()) {
                 <!-- TODO add code to pull in steps/activities -->
 
                 <div class="pl-10 text-lg">
+                    <div class="flex justify-between items-center text-xs text-slate-500 my-3">
+                        <?php
+                        $stat = 'bg-slate-200';
+                        if ($pathway->status->name == 'Draft') $stat = 'bg-orange-400 text-white';
+                        ?>
+                        <?php if ($pathway->featured == 1) : ?>
+                            <span class="bg-green-600 text-white">Featured</span>
+                        <?php endif ?>
+                        <span class="<?= $stat ?> py-1 px-2 rounded-full mr-3"><?= $pathway->status->name ?></span>
+                        <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tag inline-block" viewBox="0 0 16 16">
+                                <path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0z" />
+                                <path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1zm0 5.586 7 7L13.586 9l-7-7H2v4.586z" />
+                            </svg><?php $topiclink = $pathway->topic->categories[0]->name . ' / ' . $pathway->topic->name ?>
+                            <a href="/category/<?= $pathway->topic->categories[0]->id ?>/<?= $pathway->topic->categories[0]->slug ?>/topic/<?= $pathway->topic->id ?>/<?= $pathway->topic->slug ?>">
+                                <?= $topiclink ?>
+                            </a></span>
+
+                    </div>
                     <p><?= h($pathway->description) ?></p>
 
                     <p> <a href="/<?= h($pathway->topic->categories[0]->slug) ?>/<?= h($pathway->topic->slug) ?>/pathway/<?= h($pathway->slug) ?>" class="text-sky-700 underline">
                             View the <strong><?= h($pathway->name) ?></strong> pathway
                     </p>
                 </div>
-                <!-- TODO fix badges/tags -->
-                <div class="p-3 bg-slate-700 rounded-lg">
-                    <?php
-                    $stat = 'badge-light';
-                    if ($pathway->status->name == 'Draft') $stat = 'badge-warning';
-                    ?>
-                    <?php if ($pathway->featured == 1) : ?>
-                        <span class="badge badge-success">Featured</span>
-                    <?php endif ?>
-                    <span class="badge <?= $stat ?>"><?= $pathway->status->name ?></span> in
-                    <?php $topiclink = $pathway->topic->categories[0]->name . ', ' . $pathway->topic->name ?>
-                    <a href="/category/<?= $pathway->topic->categories[0]->id ?>/<?= $pathway->topic->categories[0]->slug ?>/topic/<?= $pathway->topic->id ?>/<?= $pathway->topic->slug ?>">
-                        <?= $topiclink ?>
-                    </a>
+                <!-- TODO can we enforce descriptions? Most of these don't have them -->
 
-                </div>
+
             <?php endforeach; ?>
         </div>
 
