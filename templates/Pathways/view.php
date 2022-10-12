9e5d8e20221dc6003bc41ed534f244e13305ae36
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-10-12 14:18:32 -0700

    add step section

--- templates/Pathways/view.php
+++ templates/Pathways/view.php
@@ -121,63 +121,67 @@ $this->assign('title', h($pathway->name));
             </script>
         </div>
 
-        <div x-data="{ openTab: 1 }" class="p-6">
-    <ul class="flex border-b">
-      <li @click="openTab = 1" class="-mb-px mr-1">
-        <a class="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" href="#">Tab 1</a>
-      </li>
-      <li @click="openTab = 2" class="mr-1">
-        <a class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">Tab 2</a>
-      </li>
-      <li @click="openTab = 3" class="mr-1">
-        <a class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">Tab 3</a>
-      </li>
-    </ul>
-    <div class="w-full pt-4">
-      <div x-show="openTab === 1">Tab #1</div>
-      <div x-show="openTab === 2">Tab #2</div>
-      <div x-show="openTab === 3">Tab #3</div>
-    </div>
-  </div>
 
-        <h2 class="text-3xl p-6 my-3 rounded-lg bg-white/80 dark:bg-slate-900/80 dark:text-white">
+
+        <!-- <h2 class="text-3xl p-6 my-3 rounded-lg bg-white/80 dark:bg-slate-900/80 dark:text-white">
             <span class="inline-block px-2 bg-slate-500 dark:bg-black text-white rounded-full">
                 <?= $stepcount ?>
             </span>
             steps along this pathway
             <span class="inline-block px-2 bg-slate-500 dark:bg-black text-white text-sm rounded-full">
                 <?= $requiredacts ?> required activities</span>
-        </h2>
+        </h2> -->
 
+    </div>
+    <?php if ($role == 'curator' || $role == 'superuser') : ?>
 
-        <?php if ($role == 'curator' || $role == 'superuser') : ?>
-
-            <div x-data="{ open: false }">
-                <button @click="open = ! open" class="inline-block px-4 py-2 text-white text-md bg-slate-700 hover:text-slate-900 focus:text-slate-900 hover:bg-slate-200 focus:bg-slate-200 focus:outline-none focus:shadow-outline hover:no-underline rounded-lg float-right">
+        <div x-data="{ open: false }">
+            <div class="pr-8 float-right">
+                <button @click="open = ! open" class="inline-block px-4 py-2 text-white text-md bg-slate-700 hover:text-slate-900 focus:text-slate-900 hover:bg-slate-200 focus:bg-slate-200 focus:outline-none focus:shadow-outline hover:no-underline rounded-lg ">
                     Add a Step
                 </button>
-                <div xcloak x-show="open" class="p-6 my-3 rounded-lg bg-white dark:bg-slate-800 dark:text-white">
+            </div>
+            <!-- TODO hide button when opened -->
+            <div class="max-w-prose pl-10">
+
+                <div xcloak x-show="open" class="outline outline-1 outline-offset-2 outline-slate-500 p-6 my-3 rounded-md block mt-10 ">
 
                     <?= $this->Form->create(null, ['url' => [
                         'controller' => 'Steps',
                         'action' => 'add'
                     ]]) ?>
                     <?php
-                    echo $this->Form->control('name', ['class' => 'block w-full px-3 py-2 m-0 bg-slate-100/80 dark:text-white dark:bg-slate-900/80 rounded-lg']);
-                    echo $this->Form->control('description', ['class' => 'block w-full px-3 py-2 m-0 bg-slate-100/80 dark:text-white dark:bg-slate-900/80 rounded-lg', 'type' => 'textarea', 'label' => 'Objective']);
+                    echo $this->Form->control('name', ['class' => 'block w-full px-3 py-2 m-0 bg-slate-100/80 rounded-lg mb-3']);
+                    echo $this->Form->control('description', ['class' => ' block w-full px-3 py-2 m-0 bg-slate-100/80 rounded-lg', 'type' => 'textarea', 'label' => 'Objective']);
                     echo $this->Form->hidden('createdby', ['value' => $uid]);
                     echo $this->Form->hidden('modifiedby', ['value' => $uid]);
                     echo $this->Form->hidden('pathways.0.id', ['value' => $pathway->id]);
                     ?>
-                    <?= $this->Form->button(__('Add Step'), ['class' => 'inline-block my-2 p-3 bg-sky-700 hover:bg-sky-800 rounded-lg text-white text-xl hover:no-underline']) ?>
+                    <?= $this->Form->button(__('Add Step'), ['class' => 'mt-3 inline-block px-4 py-2 text-white text-md bg-slate-700 hover:text-slate-900 focus:text-slate-900 hover:bg-slate-200 focus:bg-slate-200 focus:outline-none focus:shadow-outline hover:no-underline rounded-lg']) ?>
                     <?= $this->Form->end() ?>
                 </div>
             </div>
-
-
-        <?php endif ?>
-
-
+        </div>
+    <?php endif ?>
+    <div class="max-w-prose">
+        <div x-data="{ openTab: 1 }" class="p-6">
+            <ul class="flex border-b">
+                <li @click="openTab = 1" class="-mb-px mr-1">
+                    <a class="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" href="#">Tab 1</a>
+                </li>
+                <li @click="openTab = 2" class="mr-1">
+                    <a class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">Tab 2</a>
+                </li>
+                <li @click="openTab = 3" class="mr-1">
+                    <a class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">Tab 3</a>
+                </li>
+            </ul>
+            <div class="w-full pt-4">
+                <div x-show="openTab === 1">Tab #1</div>
+                <div x-show="openTab === 2">Tab #2</div>
+                <div x-show="openTab === 3">Tab #3</div>
+            </div>
+        </div>
         <?php if (!empty($pathway->steps)) : ?>
             <?php foreach ($pathway->steps as $steps) : ?>
                 <?php $requiredacts = 0; ?>
