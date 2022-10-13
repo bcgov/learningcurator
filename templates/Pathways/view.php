d1deebb313f290214633983fd4a2b5598079f596
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-10-13 08:53:35 -0700

    adjust tab hover colour, button hover

--- templates/Pathways/view.php
+++ templates/Pathways/view.php
@@ -62,7 +62,7 @@ $this->assign('title', h($pathway->name));
                 <div class="my-3">
                     <?= $this->Form->create(null, ['url' => ['controller' => 'pathways-users', 'action' => 'follow']]) ?>
                     <?= $this->Form->control('pathway_id', ['type' => 'hidden', 'value' => $pathway->id]) ?>
-                    <button class="py-2 px-4 bg-darkblue text-white rounded-lg">
+                    <button class="py-2 px-4 bg-darkblue text-white rounded-lg hover:bg-darkblue/80">
                         Follow Pathway<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-pin ml-2 inline" viewBox="0 0 16 16">
                             <path d="M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224 1.5-.5 1.5s-.5-1.224-.5-1.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A5.921 5.921 0 0 1 5 6.708V2.277a2.77 2.77 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354zm1.58 1.408-.002-.001.002.001zm-.002-.001.002.001A.5.5 0 0 1 6 2v5a.5.5 0 0 1-.276.447h-.002l-.012.007-.054.03a4.922 4.922 0 0 0-.827.58c-.318.278-.585.596-.725.936h7.792c-.14-.34-.407-.658-.725-.936a4.915 4.915 0 0 0-.881-.61l-.012-.006h-.002A.5.5 0 0 1 10 7V2a.5.5 0 0 1 .295-.458 1.775 1.775 0 0 0 .351-.271c.08-.08.155-.17.214-.271H5.14c.06.1.133.191.214.271a1.78 1.78 0 0 0 .37.282z" />
                         </svg></button>
@@ -157,7 +157,7 @@ $this->assign('title', h($pathway->name));
                     echo $this->Form->hidden('modifiedby', ['value' => $uid]);
                     echo $this->Form->hidden('pathways.0.id', ['value' => $pathway->id]);
                     ?>
-                    <?= $this->Form->button(__('Add Step'), ['class' => 'mt-3 inline-block px-4 py-2 text-white text-md bg-slate-700 hover:text-slate-900 focus:text-slate-900 hover:bg-slate-200 focus:bg-slate-200 focus:outline-none focus:shadow-outline hover:no-underline rounded-lg']) ?>
+                    <?= $this->Form->button(__('Add Step'), ['class' => 'mt-3 inline-block px-4 py-2 text-white text-md bg-slate-700 hover:text-slate-900 focus:text-slate-900 hover:bg-slate-700 focus:bg-slate-200 focus:outline-none focus:shadow-outline hover:no-underline rounded-lg']) ?>
                     <?= $this->Form->end() ?>
                 </div>
             </div>
@@ -179,10 +179,10 @@ $this->assign('title', h($pathway->name));
                     <!-- The tabs navigation -->
                     <div class="basis-1/5 flex-none">
                         <nav class="flex flex-col gap-2">
-                            <a :class="{ 'active bg-gray-500 -ml-8': tab === 'step1' }" @click.prevent="tab = 'step1'; window.location.hash = 'step1'" href="#" class=" border border-slate-200 rounded-l-lg py-3 px-6 bg-bluegreen hover:bg-gray-500 text-white hover:no-underline">Step 1</a>
-                            <a :class="{ 'active bg-gray-500 -ml-8': tab === 'step2' }" @click.prevent="tab = 'step2'; window.location.hash = 'step2'" href="#" class=" border border-slate-200 rounded-l-lg py-3 px-6 bg-bluegreen hover:bg-gray-500 text-white hover:no-underline">Step 2</a>
-                            <a :class="{ 'active bg-gray-500 -ml-8': tab === 'step3' }" @click.prevent="tab = 'step3'; window.location.hash = 'step3'" href="#" class=" border border-slate-200 rounded-l-lg py-3 px-6 bg-bluegreen hover:bg-gray-500 text-white hover:no-underline">Step 3</a>
-                            <a :class="{ 'active bg-gray-500 -ml-8': tab === 'step4' }" @click.prevent="tab = 'step4'; window.location.hash = 'step4'" href="#" class=" border border-slate-200 rounded-l-lg py-3 px-6 bg-bluegreen hover:bg-gray-500 text-white hover:no-underline">Step 4</a>
+                            <a :class="{ 'active bg-gray-500 -ml-8': tab === 'step1' }" @click.prevent="tab = 'step1'; window.location.hash = 'step1'" href="#" class=" border border-slate-200 rounded-l-lg py-3 px-6 bg-bluegreen hover:bg-bluegreen/80 text-white hover:no-underline">Step 1</a>
+                            <a :class="{ 'active bg-gray-500 -ml-8': tab === 'step2' }" @click.prevent="tab = 'step2'; window.location.hash = 'step2'" href="#" class=" border border-slate-200 rounded-l-lg py-3 px-6 bg-bluegreen hover:bg-bluegreen/80 text-white hover:no-underline">Step 2</a>
+                            <a :class="{ 'active bg-gray-500 -ml-8': tab === 'step3' }" @click.prevent="tab = 'step3'; window.location.hash = 'step3'" href="#" class=" border border-slate-200 rounded-l-lg py-3 px-6 bg-bluegreen hover:bg-bluegreen/80 text-white hover:no-underline">Step 3</a>
+                            <a :class="{ 'active bg-gray-500 -ml-8': tab === 'step4' }" @click.prevent="tab = 'step4'; window.location.hash = 'step4'" href="#" class=" border border-slate-200 rounded-l-lg py-3 px-6 bg-bluegreen hover:bg-bluegreen/80 text-white hover:no-underline">Step 4</a>
                         </nav>
                     </div>
                     <!-- The tabs content -->
