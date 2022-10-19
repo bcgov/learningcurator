9298d4f9eb5cc00bd67ff5af60c42221b2942823
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-10-19 12:36:08 -0700

    update intro text styling

--- templates/Questions/index.php
+++ templates/Questions/index.php
@@ -18,67 +18,72 @@ if ($this->Identity->isLoggedIn()) {
         <h1 class="text-white text-3xl font-bold m-auto tracking-wide">About</h1>
     </div>
 </header>
-<div class="p-6 dark:text-white">
-    <?php if ($role == 'curator' || $role == 'superuser') : ?>
-        <?= $this->Html->link(__('New Question'), ['action' => 'add'], ['class' => 'btn btn-success float-right']) ?>
-    <?php endif ?>
-    <h1 class="text-4xl">Learning Curator</h1>
-    <div class="p-3 my-3 rounded-lg activity bg-white dark:bg-slate-900/80 dark:text-white">
-        <div class="mb-6 text-2xl">
+<div class="p-8 text-lg">
+
+
+    <div class="max-w-prose">
+        <h2 class="mb-3 text-2xl text-darkblue font-semibold">What is Learning Curator?</h2>
+
+        <p class="mb-3">
             A web site where BC Public Service curators collect readings, courses,
             activities and media, and shape pathways to learning goals. Where public
-            service employees learn on their own time, at their own pace.
-        </div>
-        <p class="mb-3 text-xl">
+            service employees learn on their own time, at their own pace.</p>
+        <p class="mb-3">
             Curator pathways are organized into
-            <a href="/categories" class="underline">categories</a> and then topics.
+            <a href="/categories" class="underline font-semibold">categories</a> and then topics.
             You can explore
-            <a href="/pathways" class="underline">all the pathways</a>
+            <a href="/pathways" class="underline font-semibold">all the pathways</a>
             we have to offer and when you see one you like, you can
-            follow it. When you follow a pathway, it will be listed
-            here, so the next time you login, you can jump right to it.
+            follow it.
         </p>
         <!-- <p>Not just a big repository of important bookmarks, Curator rejects blind tagging in
     favor of a pedagogical approach where resources are structured into pathways
     which have specific objectives, and are further organized into logic steps, revealing
     a progression of concepts that blends formal and informal learning resources into a 
     single stream.</p> -->
-        <div class="w-1/2">
-            <h2 class="text-lg">Frequently Asked Questions</h2>
-            <?php foreach ($questions as $question) : ?>
-                <?php if ($question->status_id == 2) : ?>
-                    <div class="p-3 my-3 bg-white dark:bg-slate-800 rounded-lg">
-                        <details>
-                            <summary id="<?= h($question->slug) ?>"><?= h($question->title) ?></summary>
+        <?php if ($role == 'curator' || $role == 'superuser') : ?>
+            <?= $this->Html->link(__('Add New Question'), ['action' => 'add'], ['class' => 'inline-block px-4 py-2 text-white text-md bg-slate-700 hover:text-slate-900 hover:bg-slate-200 hover:no-underline rounded-lg mb-5']) ?>
+        <?php endif ?>
 
-                            <div class="p-3 bg-slate-200 dark:bg-slate-700 rounded-lg">
-                                <?= $question->content ?>
-                            </div>
 
-                            <?php if ($role == 'curator' || $role == 'superuser') : ?>
-                                <div class="btn-group mt-3">
-                                    <?= $this->Html->link(__('Edit'), ['action' => 'edit', $question->id], ['class' => 'btn btn-primary']) ?>
-                                    <?= $this->Form->postLink(__('Delete Question'), ['action' => 'delete', $question->id], ['confirm' => __('Are you sure you want to delete # {0}?', $question->id), 'class' => 'btn btn-danger']) ?>
-                                </div>
-                            <?php endif ?>
 
-                        </details>
-                    </div>
-                <?php else : ?>
-                    <?php if ($role == 'curator' || $role == 'superuser') : ?>
-                        <div class="p-3 my-3 bg-white dark:bg-slate-900/80 rounded-lg">
-                            <div><span class="badge badge-warning"><?= h($question->status->name) ?></span></div>
-                            <h2 class="text-2xl" id="<?= h($question->slug) ?>"><?= h($question->title) ?></h2>
-                            <div><?= $question->content ?></div>
+
+
+        <h3 class="text-xl">Frequently Asked Questions</h3>
+        <?php foreach ($questions as $question) : ?>
+            <?php if ($question->status_id == 2) : ?>
+                <div class="p-3 my-3 bg-white dark:bg-slate-800 rounded-lg">
+                    <details>
+                        <summary id="<?= h($question->slug) ?>"><?= h($question->title) ?></summary>
+
+                        <div class="p-3 bg-slate-200 dark:bg-slate-700 rounded-lg">
+                            <?= $question->content ?>
+                        </div>
+
+                        <?php if ($role == 'curator' || $role == 'superuser') : ?>
                             <div class="btn-group mt-3">
                                 <?= $this->Html->link(__('Edit'), ['action' => 'edit', $question->id], ['class' => 'btn btn-primary']) ?>
                                 <?= $this->Form->postLink(__('Delete Question'), ['action' => 'delete', $question->id], ['confirm' => __('Are you sure you want to delete # {0}?', $question->id), 'class' => 'btn btn-danger']) ?>
                             </div>
-                        </div>
-                    <?php endif ?>
-                <?php endif ?>
-            <?php endforeach; ?>
+                        <?php endif ?>
 
-        </div>
+                    </details>
+                </div>
+            <?php else : ?>
+                <?php if ($role == 'curator' || $role == 'superuser') : ?>
+                    <div class="p-3 my-3 bg-white dark:bg-slate-900/80 rounded-lg">
+                        <div><span class="badge badge-warning"><?= h($question->status->name) ?></span></div>
+                        <h2 class="text-2xl" id="<?= h($question->slug) ?>"><?= h($question->title) ?></h2>
+                        <div><?= $question->content ?></div>
+                        <div class="btn-group mt-3">
+                            <?= $this->Html->link(__('Edit'), ['action' => 'edit', $question->id], ['class' => 'btn btn-primary']) ?>
+                            <?= $this->Form->postLink(__('Delete Question'), ['action' => 'delete', $question->id], ['confirm' => __('Are you sure you want to delete # {0}?', $question->id), 'class' => 'btn btn-danger']) ?>
+                        </div>
+                    </div>
+                <?php endif ?>
+            <?php endif ?>
+        <?php endforeach; ?>
+
     </div>
+</div>
 </div>
