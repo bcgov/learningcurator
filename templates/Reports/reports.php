47d7edfbaee7ceeb3f8c5c98a5525a8289be6359
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-10-20 15:11:14 -0700

    update no reports styling, add icon

--- templates/Reports/reports.php
+++ templates/Reports/reports.php
@@ -77,22 +77,26 @@ if ($this->Identity->isLoggedIn()) {
 
                 <?php endif ?>
 
+
+            <?php endforeach ?>
+        <?php else : ?>
+            <div class="max-w-prose flex justify-between gap-4 border-2 p-3 rounded-md">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-info-circle-fill flex-none" viewBox="0 0 16 16">
+                        <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+                    </svg>
+                <div class="grow">
+                    <h3 class="mb-3 text-xl font-semibold">No reports filed yet</h3>
+                    <p class="mb-2">
+                        When you report an issue with an activity, it will be listed here.
+                    </p>
+                </div>
             </div>
-        <?php endforeach ?>
-    <?php else : ?>
-        <h2 class="mb-3 text-3xl">No reports filed</h2>
-        <div class="p-4 bg-white dark:bg-slate-800 rounded-lg">
-            <p class="mb-3 text-xl">
-               When your report an issue, it will be listed here.
-            </p>
 
-        </div>
+            <a href="/categories" class="inline-block p-3 mt-4 bg-sky-700 dark:bg-sky-700 text-white text-2xl hover:no-underline rounded-lg">
+                Explore Categories
+            </a>
 
-        <a href="/categories" class="inline-block p-3 mt-4 bg-sky-700 dark:bg-sky-700 text-white text-2xl hover:no-underline rounded-lg">
-            Explore Categories
-        </a>
+        <?php endif ?>
 
-    <?php endif ?>
-
-</div>
+            </div>
 </div>
