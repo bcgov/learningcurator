f7a4d2efe8205c5f491a9aee664ef877bbaf287e
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-10-21 12:23:42 -0700

    change to columns layout for masonry effect

--- templates/Activities/index.php
+++ templates/Activities/index.php
@@ -87,7 +87,7 @@ if ($this->Identity->isLoggedIn()) {
             </div>
         </div>
     </div>
-    <div class="grid lg:grid-cols-2 gap-4 items-start">
+    <div class="columns-2 gap-4">
                 <?php foreach ($activities as $activity) : ?>
                     <?php
                     // #TODO Allan move this back into the controller and simplify
@@ -100,9 +100,8 @@ if ($this->Identity->isLoggedIn()) {
                         }
                     }
                     ?>
-                    <!-- TODO Q - does this look weird with the different sized cards? Masonry type layout may be better if possible  -->
 
-                    <div class="align-self-start rounded-md bg-sagedark hover:bg-sagedark/80 p-0.5">
+                    <div class="w-full inline-block mb-4 p-0.5 rounded-md bg-sagedark hover:bg-sagedark/80 ">
                         <div class="flex flex-row justify-between">
                             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-journal-text mx-3 my-4 flex-none" viewBox="0 0 16 16">
                                 <path d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z" />
