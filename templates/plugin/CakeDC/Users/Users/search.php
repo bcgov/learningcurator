4d10ce2e23f838bf09560ee896bc5b622e546e05
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-10-24 13:42:09 -0700

    pagination minor styling changes

--- templates/plugin/CakeDC/Users/Users/search.php
+++ templates/plugin/CakeDC/Users/Users/search.php
@@ -20,28 +20,50 @@
     <form method="get" action="/users/search" class="mt-2">
         <label class="">
             <input class="px-3 py-2 m-0 border rounded-l-lg" type="search" placeholder="first or last name ..." aria-label="User Search" name="q" value="<?= h($q) ?>"></label><button class="px-3 py-2 m-0 bg-slate-400 hover:bg-slate-300 rounded-r-lg" type="submit">User Search</button>
-            <div class="inline-block ml-2 text-sky-700 hover:underline text-base"><a href="/users/search">Show All Users</a></div>
+        <div class="inline-block ml-2 text-sky-700 hover:underline text-base"><a href="/users/search">Show All Users</a></div>
     </form>
 
     <h3 class="mt-4 font-semibold">All Users</h3>
+    <p>Click on any username to see information about that user and their contributions to the Learning Curator.</p>
+    <p class="mt-3 text-base">Page <?= $this->Paginator->counter() ?></p>
+    <table class="border-collapse border border-slate-400 mt-1">
+        <thead>
+            <tr>
+                <th class="border border-slate-300 px-2 py-1 bg-slate-200 text-left">Username</th>
+                <th class="border border-slate-300 px-2 py-1 bg-slate-200 text-left">First Name</th>
+                <th class="border border-slate-300 px-2 py-1 bg-slate-200 text-left">Last Name</th>
+                <th class="border border-slate-300 px-2 py-1 bg-slate-200 text-left">Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach (${$tableAlias} as $user) : ?>
+                <tr>
+                    <td class="px-2 py-1 border border-slate-300"><?= $this->Html->link(__d('cake_d_c/users', h($user->username)), ['action' => 'view', $user->id], ['class' => 'font-weight-bold']) ?></td>
 
-    <?php foreach (${$tableAlias} as $user) : ?>
-        <div class="bg-white dark:bg-slate-900/80 my-5 p-3">
-            <div class="">
-                <?= $this->Html->link(__d('cake_d_c/users', h($user->username)), ['action' => 'view', $user->id], ['class' => 'font-weight-bold']) ?>
-                <?= h($user->first_name) ?> <?= h($user->last_name) ?> <?= h($user->email) ?>
-            </div>
-        </div>
+                    <td class="px-2 py-1 border border-slate-300">
+                        <?= h($user->first_name) ?>
+                    </td>
+                    <td class="px-2 py-1 border border-slate-300">
+                        <?= h($user->last_name) ?>
+                    </td>
+                    <td class="px-2 py-1 border border-slate-300">
+                        <a href="mailto:<?= h($user->email) ?>" class="text-sky-700 underline hover:text-sky-500"><?= h($user->email) ?></a>
+                    </td>
+                </tr>
 
-    <?php endforeach; ?>
+
+            <?php endforeach; ?>
+
+        </tbody>
+    </table>
 
     <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->prev('< ' . __d('cake_d_c/users', 'previous')) ?>
+        <ul class="flex items-center gap-2">
+            <?= $this->Paginator->prev('< ' . __d('cake_d_c/users', 'Previous')) ?>
             <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__d('cake_d_c/users', 'next') . ' >') ?>
+            <?= $this->Paginator->next(__d('cake_d_c/users', 'Next') . ' >') ?>
         </ul>
-        <p><?= $this->Paginator->counter() ?></p>
+        
     </div>
 
 
