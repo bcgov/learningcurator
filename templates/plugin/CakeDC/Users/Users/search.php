4c702feb6238f1e40acf37f8f7da4a3c555fc443
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-11-07 13:51:06 -0800

    reduce dashboard header on small screens

--- templates/plugin/CakeDC/Users/Users/search.php
+++ templates/plugin/CakeDC/Users/Users/search.php
@@ -11,7 +11,7 @@
  */
 ?>
 
-<header class="w-full h-52 bg-darkblue px-8 flex items-center">
+<header class="w-full h-32 lg:h-52 bg-darkblue px-8 flex items-center">
     <h1 class="text-white text-3xl font-bold tracking-wide">Curator Dashboard</h1>
 </header>
 <div class="p-8 text-lg" id="mainContent">
