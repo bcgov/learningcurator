4930752a8568700a4586a16b7c62bceb65f20680
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-11-09 16:25:20 -0800

    change so wording is correct for single activity

--- templates/Steps/view.php
+++ templates/Steps/view.php
@@ -190,7 +190,10 @@ foreach ($step->pathways as $pathways) {
                 <?= $step->description ?>
             </p>
             <?php if (!empty($requiredacts)) : ?>
-                <h4 class="mt-6 text-xl text-sagedark"><span class="bg-sagedark text-white rounded-lg text-lg inline-block px-2 mr-1"><?= $stepacts ?></span>Required Activities </h4>
+                <?php if (count($requiredacts) === 1) : ?>
+                    <h4 class="mt-6 text-xl text-sagedark"><span class="bg-sagedark text-white rounded-lg text-lg inline-block px-2 mr-1"><?= $stepacts ?></span>Required Activity </h4>
+                <?php else : ?>
+                    <h4 class="mt-6 text-xl text-sagedark"><span class="bg-sagedark text-white rounded-lg text-lg inline-block px-2 mr-1"><?= $stepacts ?></span>Required Activities </h4><?php endif ?>
                 <p class="text-base"><em>Launch these activities and fill in your progress bar.</em></p>
 
 
@@ -362,8 +365,11 @@ foreach ($step->pathways as $pathways) {
             ?>
             <?php if (count($supplementalacts) > 0) : ?>
                 <div class="my-3 text-center text-sm">End of required activities for this module.</div>
-                <h4 class="mt-6 text-xl text-sagedark"><span class="bg-sagedark text-white rounded-lg text-lg inline-block px-2 mr-1"><?= $supplmentalcount ?></span>Supplementary Resources </h4>
-                
+                <?php if (count($supplementalacts) === 1) : ?>
+                    <h4 class="mt-6 text-xl text-sagedark"><span class="bg-sagedark text-white rounded-lg text-lg inline-block px-2 mr-1"><?= $supplmentalcount ?></span>Supplementary Resource </h4>
+                <?php else : ?>
+                    <h4 class="mt-6 text-xl text-sagedark"><span class="bg-sagedark text-white rounded-lg text-lg inline-block px-2 mr-1"><?= $supplmentalcount ?></span>Supplementary Resources </h4>
+                <?php endif ?>
                 <p class="text-base"><em>Launching these activities does not count towards your progress along this pathway.</em></p>
                 <?php foreach ($supplementalacts as $activity) : ?>
                     <?php
@@ -404,13 +410,15 @@ foreach ($step->pathways as $pathways) {
                                                 <em>Curator says:</em><br>
                                                 <?= $activity->_joinData->stepcontext ?>
                                             <?php endif ?>
-                                            <a target="_blank" x-on:click="count++;" onclick="loadStatus();" rel="noopener" title="Launch this activity" href="/activities-users/launch?activity_id=<?= $activity->id ?>" class="inline-block my-2 p-2 bg-darkblue hover:bg-darkblue/80 rounded-lg text-white text-lg hover:no-underline">
-                                                Launch
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="inline bi bi-box-arrow-up-right" viewBox="0 0 16 16">
-                                                    <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z" />
-                                                    <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z" />
-                                                </svg>
-                                            </a>
+                                            <div class="">
+                                                <a target="_blank" x-on:click="count++;" onclick="loadStatus();" rel="noopener" title="Launch this activity" href="/activities-users/launch?activity_id=<?= $activity->id ?>" class="inline-block my-2 p-2 bg-darkblue hover:bg-darkblue/80 rounded-lg text-white text-lg hover:no-underline">
+                                                    Launch
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="inline bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                                                        <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z" />
+                                                        <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z" />
+                                                    </svg>
+                                                </a>
+                                            </div>
                                             <div x-data="{ open: false }">
                                                 <button @click="open = !open" class="text-sm text-sky-700 text-right">
                                                     <span>More info</span>
@@ -551,5 +559,4 @@ foreach ($step->pathways as $pathways) {
         </div>
         <!--end step outer box -->
     </div>
-</div>
 </div>
