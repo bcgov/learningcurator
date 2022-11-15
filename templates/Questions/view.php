c6aac06268c348e037ba347ed443ae8b0ddd099e
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-11-15 12:19:31 -0800

    add question styling

--- templates/Questions/view.php
+++ templates/Questions/view.php
@@ -1,62 +1,60 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Question $question
  */
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit Question'), ['action' => 'edit', $question->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Question'), ['action' => 'delete', $question->id], ['confirm' => __('Are you sure you want to delete # {0}?', $question->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Questions'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New Question'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="questions view content">
-            <h3><?= h($question->title) ?></h3>
-            <table>
-                <tr>
-                    <th><?= __('Title') ?></th>
-                    <td><?= h($question->title) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Slug') ?></th>
-                    <td><?= h($question->slug) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Status') ?></th>
-                    <td><?= $question->has('status') ? $this->Html->link($question->status->name, ['controller' => 'Statuses', 'action' => 'view', $question->status->id]) : '' ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Createdby Id') ?></th>
-                    <td><?= h($question->createdby_id) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('User') ?></th>
-                    <td><?= $question->has('user') ? $this->Html->link($question->user->id, ['controller' => 'Users', 'action' => 'view', $question->user->id]) : '' ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($question->id) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Created') ?></th>
-                    <td><?= h($question->created) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Modified') ?></th>
-                    <td><?= h($question->modified) ?></td>
-                </tr>
-            </table>
-            <div class="text">
-                <strong><?= __('Content') ?></strong>
-                <blockquote>
-                    <?= $this->Text->autoParagraph(h($question->content)); ?>
-                </blockquote>
-            </div>
+<header class="w-full h-32 md:h-52 bg-darkblue px-8 flex items-center">
+    <h1 class="text-white text-3xl font-bold tracking-wide">Curator Dashboard</h1>
+</header>
+<div class="p-8 text-lg" id="mainContent">
+    <div class="max-w-prose">
+        <h2 class="text-2xl text-darkblue font-semibold mb-3">View Question <span><?= $this->Html->link(__('View All Questions'), ['action' => 'index'], ['class' => 'inline-block ml-2 text-sky-700 hover:underline text-base font-normal']) ?></span></h2>
+
+
+        <h3 class="font-semibold text-xl mt-5">Question</h3>
+        <p><?= h($question->title) ?></p>
+        <h3 class="font-semibold text-xl mt-5">Answer</h3>
+        <div><?= $question->content ?></div>
+        <h3 class="font-semibold text-xl mt-5">Details</h3>
+        <ul class="list-disc pl-8 mt-2">
+            <li class="px-2">
+                <strong>Title: </strong><?= h($question->title) ?>
+            </li>
+            <li class="px-2">
+                <strong>Slug: </strong><?= h($question->slug) ?>
+            </li>
+            <li class="px-2">
+                <strong>Status: </strong><?= $question->has('status') ? $this->Html->link($question->status->name, ['controller' => 'Statuses', 'action' => 'view', $question->status->id]) : '' ?>
+            </li>
+            <!-- TODO this link to status doesn't go anywhere -->
+            <li class="px-2">
+                <strong>Created By: </strong><?= h($question->createdby_id) ?>
+            </li>
+            <li class="px-2">
+                <strong>User: </strong><?= $question->has('user') ? $this->Html->link($question->user->id, ['controller' => 'Users', 'action' => 'view', $question->user->id]) : '' ?>
+            </li>
+            <!-- TODO display the user name instead of ID ? -->
+
+            <li class="px-2">
+                <strong>Number: </strong><?= $this->Number->format($question->id) ?>
+            </li>
+
+            <li class="px-2">
+                <strong>Created: </strong><?= h($question->created) ?>
+            </li>
+
+            <li class="px-2">
+                <strong>Modified: </strong><?= h($question->modified) ?>
+            </li>
+
+        </ul>
+
+        <div class="mt-4">
+            <?= $this->Html->link(__('Edit Question'), ['action' => 'edit', $question->id], ['class' => 'inline-block px-3 py-1 text-white text-base bg-slate-700 hover:text-slate-900 hover:bg-slate-200 hover:no-underline rounded-lg']) ?>
+            <?= $this->Form->postLink(__('Delete Question'), ['action' => 'delete', $question->id], ['confirm' => __('Are you sure you want to delete # {0}?', $question->id), 'class' => 'inline-block px-3 py-1 text-base hover:bg-red-700/80 text-white bg-red-700 hover:no-underline rounded-lg']) ?>
+            <?= $this->Html->link(__('Add New Question'), ['action' => 'add'], ['class' => 'inline-block px-3 py-1 text-white text-base bg-slate-700 hover:text-slate-900 hover:bg-slate-200 hover:no-underline rounded-lg']) ?>
         </div>
     </div>
 </div>
