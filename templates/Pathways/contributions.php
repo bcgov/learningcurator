6a17f5722f45830a6491c2bb1b1b09574c7050a9
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-11-15 16:34:58 -0800

    update activity card styling - date added

--- templates/Pathways/contributions.php
+++ templates/Pathways/contributions.php
@@ -24,43 +24,47 @@ if ($this->Identity->isLoggedIn()) {
     <div class="max-w-prose">
         <?php if (!$pathways->all()->isEmpty()) : ?>
             <?php foreach ($pathways as $pathway) : ?>
+                <div class="mb-8">
 
-                <a href="/pathways/<?= h($pathway->slug) ?>" class="hover:no-underline">
+                    <a href="/pathways/<?= h($pathway->slug) ?>" class="hover:no-underline">
 
-                    <div class="pl-2 pr-3 py-2 mt-3 mb-8 bg-bluegreen text-white  hover:bg-bluegreen/80  w-full rounded-l-full flex items-center justify-between">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-signpost-2 inline-block mx-3 flex-none" viewBox="0 0 16 16">
-                            <path d="M7 1.414V2H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h5v1H2.5a1 1 0 0 0-.8.4L.725 8.7a.5.5 0 0 0 0 .6l.975 1.3a1 1 0 0 0 .8.4H7v5h2v-5h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H9V6h4.5a1 1 0 0 0 .8-.4l.975-1.3a.5.5 0 0 0 0-.6L14.3 2.4a1 1 0 0 0-.8-.4H9v-.586a1 1 0 0 0-2 0zM13.5 3l.75 1-.75 1H2V3h11.5zm.5 5v2H2.5l-.75-1 .75-1H14z" />
-                        </svg>
-                        <h3 class="text-2xl flex-1">
-                            <?= h($pathway->name) ?>
-                        </h3>
-                        <?php if ($pathway->status_id == 1) : ?>
-                            <span class="bg-orange-400 text-slate-900 rounded-full px-2 py-1 text-sm" title="Edit to set to publish">DRAFT</span>
-                        <?php endif ?>
-                        <?php if ($pathway->featured == 1) : ?>
-                            <span class="bg-green-500 text-slate-900 rounded-full px-2 py-1 text-sm">Featured</span>
-                        <?php endif ?>
-                        <!-- <span class="text-sm justify-self-end flex-none">8 steps | 23 activities</span> -->
+                        <div class="pl-2 pr-3 py-2 mt-3  bg-bluegreen text-white  hover:bg-bluegreen/80  w-full rounded-l-full flex items-center justify-between">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-signpost-2 inline-block mx-3 flex-none" viewBox="0 0 16 16">
+                                <path d="M7 1.414V2H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h5v1H2.5a1 1 0 0 0-.8.4L.725 8.7a.5.5 0 0 0 0 .6l.975 1.3a1 1 0 0 0 .8.4H7v5h2v-5h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H9V6h4.5a1 1 0 0 0 .8-.4l.975-1.3a.5.5 0 0 0 0-.6L14.3 2.4a1 1 0 0 0-.8-.4H9v-.586a1 1 0 0 0-2 0zM13.5 3l.75 1-.75 1H2V3h11.5zm.5 5v2H2.5l-.75-1 .75-1H14z" />
+                            </svg>
+                            <h3 class="text-2xl flex-1">
+                                <?= h($pathway->name) ?>
+                            </h3>
+                            <?php if ($pathway->status_id == 1) : ?>
+                                <span class="bg-orange-400 text-slate-900 rounded-full px-2 py-1 text-sm" title="Edit to set to publish">DRAFT</span>
+                            <?php endif ?>
+                            <?php if ($pathway->featured == 1) : ?>
+                                <span class="bg-green-500 text-slate-900 rounded-full px-2 py-1 text-sm">Featured</span>
+                            <?php endif ?>
+                            <!-- <span class="text-sm ml-3 justify-self-end flex-none"><?= $pathway->stepcount ?> steps | <?= $pathway->requiredacts ?> activities</span> -->
+                            <!-- TODO fix steps/activity count here -->
+                        </div>
+                    </a>
+                    <div class="pl-10">
+                        <div class="flex justify-end items-center text-xs text-slate-500 mt-2 mb-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tag inline-block mr-1" viewBox="0 0 16 16">
+                                <path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0z" />
+                                <path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1zm0 5.586 7 7L13.586 9l-7-7H2v4.586z" />
+                            </svg>
+
+                            <?= $this->Html->link($pathway->topic->name, ['controller' => 'Topics', 'action' => 'view', $pathway->topic->id], ['class' => '']) ?>
+                            <!-- TODO Nori Allan add code to link category as well as topic -->
+                        </div>
+                        <p class="mb-3"><?php if (!empty($pathway->description)) : ?>
+                                <?= $pathway->description ?>
+                            <?php else : ?>
+                                <?= $pathway->objective ?>
+                            <?php endif ?></p>
+                        <!-- This conditional is kind of a hack and we need to make people aware that the description isn't actually optional -->
+                        <p class="mb-4"> <a href="/pathways/<?= h($pathway->slug) ?>" class="text-sky-700 underline">
+                                View the <strong><?= h($pathway->name) ?></strong> pathway
+                            </a> </p>
                     </div>
-                </a>
-                <div class="pl-10">
-                    <div class="flex justify-end items-center text-xs text-slate-500 mt-2 mb-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tag inline-block mr-1" viewBox="0 0 16 16">
-                            <path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0z" />
-                            <path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1zm0 5.586 7 7L13.586 9l-7-7H2v4.586z" />
-                        </svg>
-                        <?= $this->Html->link($pathway->topic->name, ['controller' => 'Topics', 'action' => 'view', $pathway->topic->id], ['class' => '']) ?>
-                        <!-- TODO Nori Allan add code to link category as well as topic -->
-                    </div>
-                    <p class="mb-3"><?php if (!empty($pathway->description)) : ?>
-                            <?= $pathway->description ?>
-                        <?php else : ?>
-                            <?= $pathway->objective ?>
-                        <?php endif ?></p>
-                    <!-- This conditional is kind of a hack and we need to make people aware that the description isn't actually optional -->
-                    <p class="mb-4"> <a href="/pathways/<?= h($pathway->slug) ?>" class="text-sky-700 underline">
-                            View the <strong><?= h($pathway->name) ?></strong> pathway
-                        </a> </p>
                 </div>
 
             <?php endforeach; ?>
@@ -96,12 +100,11 @@ if ($this->Identity->isLoggedIn()) {
                             <div class="p-3 text-lg">
 
                                 <?php if ($a->status->name == 'Draft') : ?>
-                                    <span class="bg-orange-400 text-slate-900 rounded-full px-2 py-0.5 text-sm float-right" title="Edit to set to publish">DRAFT</span>
+                                    <span class="bg-orange-400 text-slate-900 rounded-full px-2 py-0.5 text-sm" title="Edit to set to publish">DRAFT</span>
                                 <?php endif ?>
                                 <h4 class="mb-3 mt-1 text-xl font-semibold">
                                     <a href="/activities/view/<?= $a->id ?>"><?= h($a->name) ?></a>
                                 </h4>
-                                <!-- TODO Nori/Allan add created/modified date? -->
                                 <div class="">
                                     <?php if (!empty($a->description)) : ?>
                                         <?= $a->description ?>
@@ -115,30 +118,43 @@ if ($this->Identity->isLoggedIn()) {
                                     <div class="text-sm italic mt-2">
                                         Curator says:
                                         <blockquote class="border-l-2 p-2 m-2"><?= $a->_joinData->stepcontext ?></blockquote>
-                                    </div><?php endif ?>
+                                    </div>
+                                <?php endif ?>
 
-                                
 
-                                <?php if (!empty($a->steps)) : ?> in <?php endif ?>
-                                <?php foreach ($a->steps as $step) : ?>
-                                    <?php if (!empty($step->pathways[0]->slug)) : ?>
-                                        <a href="/pathways/<?= h($step->pathways[0]->slug) ?>/s/<?= $step->id ?>/<?= $step->slug ?>">
-                                            <?= h($step->pathways[0]->name) ?> - <?= h($step->name) ?>
-                                        </a>
-                                    <?php endif ?>
-                                <?php endforeach ?>
+
+                                <div class="text-sm mt-2">
+                                    <?php if (!empty($a->steps)) : ?>
+
+                                        <p class="mb-0 inline">
+                                            <strong><?= $a->status->name ?></strong>
+                                            in
+                                        </p><?php endif ?>
+                                    <?php foreach ($a->steps as $step) : ?>
+                                        <?php if (!empty($step->pathways[0]->slug)) : ?>
+                                            <a href="/pathways/<?= h($step->pathways[0]->slug) ?>/s/<?= $step->id ?>/<?= $step->slug ?>">
+                                                <?= h($step->pathways[0]->name) ?> - <?= h($step->name) ?>
+                                            </a>
+                                        <?php endif ?>
+                                    <?php endforeach ?>
+
+                                    <p class="mb-0"><strong>Added:</strong> <?= $this->Time->format($a->created, \IntlDateFormatter::MEDIUM, null, 'GMT-8') ?></p>
+
+
+
+                                </div>
                             </div>
                         </div>
-                    <?php endforeach ?>
-                    </div>
-                <?php else : ?>
-                    <div class="mt-2">
-                        <p>No activities contributed yet.</p>
-                        <a class="px-4 py-2 text-white text-md bg-slate-700 hover:text-slate-900 hover:bg-slate-200 hover:no-underline rounded-lg inline-block" href="/activities/add">Add an Activity</a>
-                    </div>
-                <?php endif ?>
+                    </div> <?php endforeach ?>
                 </div>
-
-
         </div>
+    <?php else : ?>
+        <div class="mt-2">
+            <p>No activities contributed yet.</p>
+            <a class="px-4 py-2 text-white text-md bg-slate-700 hover:text-slate-900 hover:bg-slate-200 hover:no-underline rounded-lg inline-block" href="/activities/add">Add an Activity</a>
+        </div>
+    <?php endif ?>
+
+
+
 </div>
