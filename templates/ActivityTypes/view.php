9a9b33296812f7044d8b8da7f71849158fb9d408
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-11-21 09:29:01 -0800

    add link to view all

--- templates/ActivityTypes/view.php
+++ templates/ActivityTypes/view.php
@@ -13,10 +13,10 @@
     <h2 class="text-2xl text-darkblue font-semibold mb-3">View Activity Type: <span class="text-slate-900">
             <!-- <span class="fas <?= h($activityType->image_path) ?>"></span> -->
             <?= h($activityType->name) ?>
-        </span></h2>
+        </span> <a href="/activity-types/" class="inline-block ml-2 text-sky-700 hover:underline text-base font-normal">View all Activity Types</a></h2>
     <!-- TODO alphabetize the list or sort options? -->
     <div class="mt-2 mb-3 text-xl"><?= $activityType->description; ?></div>
-    
+
     <?= $this->Html->link(('Edit the ' . $activityType->name . ' activity type'), ['controller' => 'ActivityTypes', 'action' => 'edit', $activityType->id], ['class' => 'mt-3 mb-5 inline-block px-4 py-2 text-white text-md bg-slate-700 hover:text-slate-900 focus:text-slate-900 hover:bg-slate-200 focus:bg-slate-200 focus:outline-none focus:shadow-outline hover:no-underline rounded-lg']) ?>
 
     <ul class="pl-8 list-disc">
