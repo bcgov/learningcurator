cd3d0f089861a1697342416248da1b627971eaca
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-11-21 14:28:09 -0800

    make consistent with index

--- templates/ActivityTypes/view.php
+++ templates/ActivityTypes/view.php
@@ -10,14 +10,19 @@
     <h1 class="text-white text-3xl font-bold tracking-wide">Curator Dashboard</h1>
 </header>
 <div class="p-8 text-lg" id="mainContent">
-    <h2 class="text-2xl text-darkblue font-semibold mb-3">View Activity Type: <span class="text-slate-900">
-            <!-- <span class="fas <?= h($activityType->image_path) ?>"></span> -->
-            <?= h($activityType->name) ?>
-        </span> <a href="/activity-types/" class="inline-block ml-2 text-sky-700 hover:underline text-base font-normal">View all Activity Types</a></h2>
+    <h2 class="text-2xl text-darkblue font-semibold mb-1">View Activity Type</h2>
     <!-- TODO alphabetize the list or sort options? -->
-    <div class="mt-2 mb-3 text-xl"><?= $activityType->description; ?></div>
+    <div class="mt-0 mb-3 text-xl">
+        <h3 class="font-semibold text-2xl"><i class="<?= h($activityType->image_path) ?> text-2xl mr-2" aria-hidden="true"></i><?= h($activityType->name) ?></h3>
+        <?= $activityType->description; ?>
+    </div>
+    <div class="mt-2">
+        <a title="View this activity type" href="/activity-types/" class="mb-3 inline-block px-3 py-1 text-white text-md bg-slate-700 hover:text-slate-900 focus:text-slate-900 hover:bg-slate-200 focus:bg-slate-200 focus:outline-none focus:shadow-outline hover:no-underline rounded-lg">
+            View All Types
+        </a>
+        <?= $this->Html->link(('Edit'), ['controller' => 'ActivityTypes', 'action' => 'edit', $activityType->id], ['class' => 'mt-3 mb-5 inline-block px-3 py-1 text-white text-md bg-slate-700 hover:text-slate-900 focus:text-slate-900 hover:bg-slate-200 focus:bg-slate-200 focus:outline-none focus:shadow-outline hover:no-underline rounded-lg']) ?>
+    </div>
 
-    <?= $this->Html->link(('Edit the ' . $activityType->name . ' activity type'), ['controller' => 'ActivityTypes', 'action' => 'edit', $activityType->id], ['class' => 'mt-3 mb-5 inline-block px-4 py-2 text-white text-md bg-slate-700 hover:text-slate-900 focus:text-slate-900 hover:bg-slate-200 focus:bg-slate-200 focus:outline-none focus:shadow-outline hover:no-underline rounded-lg']) ?>
 
     <ul class="pl-8 list-disc">
         <?php foreach ($activities as $activity) : ?>
