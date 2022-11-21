625760433fd833ab8db2427e4d54946585c113e0
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-11-21 09:39:00 -0800

    update styling for activity type page

--- templates/ActivityTypes/edit.php
+++ templates/ActivityTypes/edit.php
@@ -11,40 +11,31 @@
 <div class="p-8 text-lg" id="mainContent">
     <h2 class="text-2xl text-darkblue font-semibold mb-3">Edit Activity Type: <span class="text-slate-900"><a href="/activity-types/view/<?= $activityType->id ?>">
                 <?= h($activityType->name) ?></a>
-        </span></h2>
+        </span> <a href="/activity-types/" class="inline-block ml-2 text-sky-700 hover:underline text-base font-normal">View all Activity Types</a></h2>
     <div class="max-w-prose">
         <div class="outline outline-1 outline-offset-2 outline-slate-500 p-6 my-3 rounded-md block">
-            <div class="row">
-                <aside class="column">
-                    <div class="side-nav">
-                        <h4 class="heading"><?= __('Actions') ?></h4>
-                        <?= $this->Form->postLink(
-                            __('Delete'),
-                            ['action' => 'delete', $activityType->id],
-                            ['confirm' => __('Are you sure you want to delete # {0}?', $activityType->id), 'class' => 'side-nav-item']
-                        ) ?>
-                        <?= $this->Html->link(__('List Activity Types'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-                    </div>
-                </aside>
-                <div class="column-responsive column-80">
-                    <div class="activityTypes form content">
-                        <?= $this->Form->create($activityType) ?>
-                        <fieldset>
-                            <legend><?= __('Edit Activity Type') ?></legend>
-                            <?php
-                            echo $this->Form->control('name');
-                            echo $this->Form->control('slug');
-                            echo $this->Form->control('description');
-                            echo $this->Form->control('color');
-                            echo $this->Form->control('delivery_method');
-                            echo $this->Form->control('image_path');
-                            echo $this->Form->control('featured');
-                            echo $this->Form->control('createdby');
-                            echo $this->Form->control('modifiedby');
-                            ?>
-                        </fieldset>
-                        <?= $this->Form->button(__('Submit')) ?>
-                        <?= $this->Form->end() ?>
-                    </div>
-                </div>
-            </div>
+
+            <?= $this->Form->create($activityType) ?>
+            <fieldset>
+                <?php
+                echo $this->Form->control('name', ['class' => 'form-field text-base mb-3']);
+                // echo $this->Form->control('slug', ['class' => 'form-field text-base mb-3']);
+                echo $this->Form->control('description', ['class' => 'form-field text-base mb-3']);
+                // echo $this->Form->control('color', ['class' => 'form-field text-base mb-3']);
+                // echo $this->Form->control('delivery_method', ['class' => 'form-field text-base mb-3']);
+                echo $this->Form->control('image_path', ['class' => 'form-field text-base mb-3']);
+                echo $this->Form->control('featured', ['class' => 'form-field text-base mb-3']);
+                echo $this->Form->control('createdby', ['label' => 'Created By', 'class' => 'form-field text-base mb-3']);
+                echo $this->Form->control('modifiedby', ['label' => 'Modified By', 'class' => 'form-field text-base mb-3']);
+                ?>
+            </fieldset>
+            <?= $this->Form->button(__('Save Activity Type'), ['class' => 'mt-3 inline-block px-4 py-2 text-white text-md bg-slate-700 hover:text-slate-900 focus:text-slate-900 hover:bg-slate-200 focus:bg-slate-200 focus:outline-none focus:shadow-outline hover:no-underline rounded-lg']) ?>
+            <?= $this->Form->end() ?>
+            <?= $this->Form->postLink(
+                __('Delete Activity Type'),
+                ['action' => 'delete', $activityType->id],
+                ['confirm' => __('Are you sure you want to delete # {0}?', $activityType->id), 'class' => 'block my-2 text-red-500 underline hover:text-red-700 hover:cursor-pointer text-base']
+            ) ?>
+        </div>
+    </div>
+</div>
