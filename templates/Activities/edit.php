9540c1b9f0259bc83f4a8ebb50c2eac97b183c82
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-11-21 15:52:50 -0800

    styling for edit activity page

--- templates/Activities/edit.php
+++ templates/Activities/edit.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Activity $activity
@@ -6,87 +7,86 @@
 
 $this->loadHelper('Authentication.Identity');
 ?>
-<div class="p-6">
-<div class="p-3 bg-slate-100/80 dark:bg-slate-900/80 rounded-lg">
+<header class="w-full h-32 md:h-52 bg-darkblue px-8 flex items-center">
+    <h1 class="text-white text-3xl font-bold tracking-wide">Curator Dashboard</h1>
+</header>
+<div class="p-8 text-lg" id="mainContent">
+    <h2 class="text-2xl text-darkblue font-semibold mb-3">Edit Activity: <span class="text-slate-900"><a href="/activities/view/<?= $activity->id ?>"><?= $activity->name ?></a></span></h2>
+    <div class="max-w-prose">
+        <div class="outline outline-1 outline-offset-2 outline-slate-500 p-6 my-3 rounded-md block">
+            <?= $this->Form->create($activity) ?>
+            <?php
+            // echo $this->Form->control('ministry_id', ['class' => 'form-field mb-3', 'options' => $ministries, 'empty' => true]);
+            // echo $this->Form->control('category_id', ['class' => 'form-field mb-3', 'options' => $categories, 'empty' => true]);
+            // echo $this->Form->control('approvedby_id', ['class' => 'form-field mb-3']);
+            echo $this->Form->hidden('createdby_id', ['value' => $activity->createdby_id, 'class' => 'form-field mb-3']);
+            echo $this->Form->hidden('modifiedby_id', ['value' => $this->Identity->get('id'), 'class' => 'form-field mb-3']);
+            ?>
+            <?php echo $this->Form->control('name', ['class' => 'form-field mb-3']); ?>
+            <?php echo $this->Form->control('description', ['class' => 'form-field mb-3']); ?>
+            <?php echo $this->Form->control('hyperlink', ['class' => 'form-field mb-3']); ?>
+            <div id="linkcheck"></div>
+            <?php
+            if ($this->Form->isFieldError('hyperlink')) {
+                echo $this->Form->error('hyperlink', 'This link may already exist in the system.');
+            }
+            ?>
+            <?php //echo $this->Form->control('steps._ids', ['class' => 'form-field mb-3', 'options' => $steps]); 
+            ?>
+            <?php //echo $this->Form->control('licensing', ['class' => 'form-field mb-3']); 
+            ?>
+            <?php //echo $this->Form->control('moderator_notes', ['class' => 'form-field mb-3']); 
+            ?>
+            <?php echo $this->Form->control('isbn', ['class' => 'form-field mb-3', 'label' => 'ISBN']); ?>
+            <?php echo $this->Form->control('activity_types_id', ['class' => 'form-field mb-3 text-base', 'options' => $activityTypes]); ?>
+            <?php echo $this->Form->control('status_id', ['class' => 'form-field mb-3 text-base', 'options' => $statuses, 'empty' => true]); ?>
+            <?php //echo $this->Form->control('estimated_time', ['type' => 'text', 'label' => 'Estimated Time', 'class' => 'form-field mb-3']); 
+            ?>
+            <label>Estimated Time
+                <select name="estimated_time" id="estimated_time_id" class="form-field text-base">
+                    <option>Under 5 mins</option>
+                    <option>Under 10 mins</option>
+                    <option>Under 15 mins </option>
+                    <option>Under 20 mins</option>
+                    <option>Under 30 mins</option>
+                    <option>Under 1 hour</option>
+                    <option>Half day or less</option>
+                    <option>1 day </option>
+                    <option>More than 1 day </option>
+                    <option>Variable</option>
+                </select>
+            </label>
+            <?php //echo $this->Form->control('tag_string', ['class' => 'form-field mb-3', 'type' => 'text', 'label' => 'Tags']); 
+            ?>
+            <?php //echo $this->Form->control('users._ids', ['class' => 'form-field mb-3', 'options' => $users]); 
+            ?>
+            <?php //echo $this->Form->control('competencies._ids', ['class' => 'form-field mb-3', 'options' => $competencies]); 
+            ?>
+            <?= $this->Form->button(__('Save Activity'), ['class' => 'mt-3 inline-block px-4 py-2 text-white text-md bg-slate-700 hover:text-slate-900 focus:text-slate-900 hover:bg-slate-200 focus:bg-slate-200 focus:outline-none focus:shadow-outline hover:no-underline rounded-lg']) ?>
+            <?= $this->Form->end() ?>
 
-<?= $this->Form->create($activity) ?>
-<?php 
-// echo $this->Form->control('ministry_id', ['class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg', 'options' => $ministries, 'empty' => true]);
-// echo $this->Form->control('approvedby_id', ['class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg']);
-//echo $this->Form->hidden('createdby_id', ['value' => $activity->createdby_id, 'class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg']);
-echo $this->Form->hidden('modifiedby_id', ['value' => $this->Identity->get('id'),'class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg']);
-?>
+        </div>
+    </div> <?php if (!empty($activity->steps)) : ?>
+        <h3 class="font-semibold text-xl mt-5">Related Pathways</h3>
+        <p class="mb-2">This activity is included in the following pathways:</p>
+        <ul class="list-disc pl-8">
+            <?php foreach ($activity->steps as $step) : ?>
+                <?php foreach ($step->pathways as $path) : ?>
+                    <?php if ($path->status_id == 2) : ?>
+                        <li class="px-2">
+                            <a href="/pathways/<?= $path->slug ?>/s/<?= $step->id ?>/<?= $step->slug ?>"><?= $path->name ?> - <?= $step->name ?></a>
+                        </li>
 
-<h1 class="text-3xl">
-Editing <a href="/activities/view/<?= $activity->id ?>"><?= $activity->name ?></a>
-</h1>
-<!-- <div class="alert alert-light"><em><?= $activity->slug ?></em></div> -->
-<a href="/activities/view/<?= $activity->id ?>" class="inline-block my-2 p-3 bg-sky-700 hover:bg-sky-800 rounded-lg text-white text-xl hover:no-underline">
-    View Activity
-</a>
+                    <?php else : ?>
+                        <li class="px-2">
+                            <span class="bg-orange-500 text-slate-900 py-1 px-2 rounded-full mr-3">DRAFT</span>
+                            <a href="/pathways/<?= $path->slug ?>/s/<?= $step->id ?>/<?= $step->slug ?>"><?= $path->name ?> - <?= $step->name ?></a>
+                        </li>
 
-<?php echo $this->Form->control('status_id', ['class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg', 'options' => $statuses, 'empty' => true]); ?>
-<?php echo $this->Form->control('featured', ['type' => 'checkbox', 'class' => '']); ?>
+                    <?php endif ?>
+                <?php endforeach ?>
+            <?php endforeach ?>
+        </ul>
+    <?php endif ?>
 
-<?php echo $this->Form->control('name', ['class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 text-2xl rounded-lg']); ?>
-<?php echo $this->Form->control('hyperlink', ['class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg']); ?>
-<?php echo $this->Form->control('description', ['class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg']); ?>
-<?php echo $this->Form->control('createdby_id', ['type' => 'select', 'options' => $users, 'class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg', 'label' => 'Curated By']) ?>
-
-<?php echo $this->Form->control('activity_types_id', ['class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg', 'options' => $activityTypes]); ?>
-
-<?php //echo $this->Form->control('estimated_time', ['type' => 'text', 'label' => 'Estimated Time', 'class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg']); ?>
-<label>Estimated Time
-<select name="estimated_time" id="estimated_time_id" class="block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg">
-<?php
-$options = array(
-'Under 5 mins',
-'Under 10 mins', 
-'Under 15 mins', 
-'Under 20 mins',
-'Under 10 mins',
-'Under 30 mins',
-'Under 1 hour',
-'Half day or less',
-'1 day',
-'More than 1 day',
-'Variable');
-?>
-<?php foreach($options as $o): ?>
-<?php if($o == $activity->estimated_time): ?>
-<option selected><?= $o ?></option>
-<?php else: ?>
-<option><?= $o ?></option>
-<?php endif ?>
-<?php endforeach ?>
-</select>
-</label>
-
-
-<?php //echo $this->Form->control('moderation_flag', ['type' => 'checkbox', 'class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg']); ?>
-
-<?php //echo $this->Form->control('recommended', ['type' => 'text', 'class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg']); ?>
-
-<?php //echo $this->Form->control('steps._ids', ['class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg', 'options' => $steps]); ?>
-<?php //echo $this->Form->control('licensing', ['class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg']); ?>
-<?php //echo $this->Form->control('moderator_notes', ['class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg']); ?>
-<?php echo $this->Form->control('isbn', ['class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg', 'label' => 'ISBN']); ?>
-
-
-<?php echo $this->Form->control('tag_string', ['class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg', 'type' => 'text']); ?>
-<?php //echo $this->Form->control('users._ids', ['class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg', 'options' => $users]); ?>
-<?php //echo $this->Form->control('competencies._ids', ['class' => 'block w-full px-3 py-2 m-0 dark:text-white dark:bg-slate-800 rounded-lg', 'options' => $competencies]); ?>
-<?= $this->Form->button(__('Save Activity'), ['class' => 'btn btn-block btn-success my-3']) ?>
-<h2>Pathways</h2>
-<?php foreach($activity->steps as $s): ?>
-<div>
-<a href="/pathways/<?= $s->pathways[0]['slug'] ?>/s/<?= $s->id ?>/<?= $s->slug ?>">
-<?= $s->pathways[0]['name'] ?> - <?= $s->name ?>
-</a>
-</div>
-<?php endforeach ?>
-
-<?= $this->Form->end() ?>
-
-</div>
 </div>
