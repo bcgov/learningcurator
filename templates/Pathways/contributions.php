219fc314e84940b31d67ed95e6b66509513ac2c0
Nori Sinclair <nori.sinclair@gov.bc.ca>
2022-12-09 15:58:24 -0800

    change to activity type icons

--- templates/Pathways/contributions.php
+++ templates/Pathways/contributions.php
@@ -90,12 +90,7 @@ if ($this->Identity->isLoggedIn()) {
             <?php foreach ($activities as $a) : ?>
                 <div class="w-full inline-block mb-4 p-0.5 mt-2 rounded-md bg-sagedark hover:bg-sagedark/80 ">
                     <div class="flex flex-row justify-between">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-journal-text mx-3 my-4 flex-none" viewBox="0 0 16 16">
-                            <path d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z" />
-                            <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z" />
-                            <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z" />
-                        </svg>
-                        <!-- TODO Allan Change icon for activity based on activity type -->
+                    <i class="<?= h($a->activity_type->image_path) ?> mx-3 my-4 flex-none" style="color:white; font-size: 2rem;" aria-label="<?= h($a->activity_type->name) ?>"></i>
                         <div class="bg-white inset-1 rounded-r-sm flex-1">
                             <div class="p-3 text-lg">
 
