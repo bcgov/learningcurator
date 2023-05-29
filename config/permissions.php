ba19f4d32c92f372407751efb372cc80bd89171e
Allan Haggett <allan.haggett@gov.bc.ca>
2023-05-29 15:41:53 -0700

    fix permissions for addtopath bookmarklet close 308

--- config/permissions.php
+++ config/permissions.php
@@ -187,7 +187,7 @@ return [
         [
             'role' => 'curator',
             'controller' => 'Activities',
-            'action' => ['edit','delete','add','addtostep','stepfind','getinfo','linkcheck','audit','flagged'],
+            'action' => ['edit','delete','add','addtopath','addacttostep','stepfind','getinfo','linkcheck','audit','flagged'],
         ],
         [
             'role' => 'curator',
