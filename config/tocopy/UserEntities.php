5907f8a5f54d256ce6204137a119a8b33c27c8e7
Allan Haggett <allan.haggett@gov.bc.ca>
2023-09-10 14:21:10 -0700

    Set ID field to be updateable so not autogenerated Use BC Gov GUID instead!

--- config/tocopy/UserEntities.php
+++ config/tocopy/UserEntities.php
@@ -40,7 +40,7 @@ class User extends Entity
      */
     protected $_accessible = [
         '*' => true,
-        'id' => false,
+        'id' => true,
         'is_superuser' => false,
         'role' => true,
     ];
