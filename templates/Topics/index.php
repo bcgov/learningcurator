9a27e7d5ffe92af2e7eb91c7bb933dca52e12c14
Allan Haggett <allan.haggett@gov.bc.ca>
2023-10-02 15:47:30 -0700

    Updating the edges

--- templates/Topics/index.php
+++ templates/Topics/index.php
@@ -23,12 +23,20 @@
 <div class="">
     <div>
         <a href="/topic/<?= h($topic->slug) ?>" class="block text-xl pl-2 pr-3 py-2 mb-3 mt-8 bg-bluegreen text-white hover:bg-bluegreen/80  w-full rounded-lg">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tag inline-block mr-1" viewBox="0 0 16 16">
+                <path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0z" />
+                <path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1zm0 5.586 7 7L13.586 9l-7-7H2v4.586z" />
+            </svg>
             <?= h($topic->name) ?>
         </a>
     </div>
     <div class="px-3">
         <?= h($topic->description) ?>
-        <div class="mt-3 font-bold"><?php echo count($topic->pathways) ?> Pathways</div>
+        <div class="mt-3 font-bold">
+            <a href="/topic/<?= h($topic->slug) ?>">
+                <?php echo count($topic->pathways) ?> Pathways
+            </a>
+        </div>
     </div>
 </div>
 <?php endforeach; ?>
